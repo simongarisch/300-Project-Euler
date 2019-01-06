4de4ad0f4c01e8d3ba5f90d08dc488dc73ee8987
simongarisch <gatman946@gmail.com>
2019-01-06 20:28:42 +1100

    Adding C# example for Ex3.

--- exercises/Ex3/C#.ex3.cs
+++ exercises/Ex3/C#.ex3.cs
@@ -1,19 +1,38 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
 */
 
 using System;
 
-class Ex1{
+class Ex3{
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
+    long largest = 0;            // the largest prime factor
+    long target = 600851475143;  // for a target number
+
+    for(long i=2; i<Math.Pow(target, 0.5); i++){
+      if(target % i == 0){  // it's a factor
+        if(is_prime(i)){    // and it's a prime
+          largest = i;
+        }
       }
     }
-    Console.WriteLine(sum); // 233,168
+
+    Console.WriteLine(largest);  // 6,857
   }
+
+  static bool is_prime(long x){
+    // will return true if x is prime, false otherwise
+    if(x == 2 || x == 3){
+      return true;
+    }
+    for(long i=2; i<x; i++){
+      if(x % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
