8bfb28da5e0744461741b6049215de47ff4cfb8b
simongarisch <gatman946@gmail.com>
2019-01-07 08:38:17 +1100

    Adding C++ example for Ex4.

--- exercises/Ex4/c++.ex4.cpp
+++ exercises/Ex4/c++.ex4.cpp
@@ -1,18 +1,44 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A palindromic number reads the same both ways. The largest palindrome made from
+the product of two 2-digit numbers is 9009 = 91 * 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+bool is_palindromic_number(long int x);
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  long int result, largest = 0;
+  // go through pairs of three digit numbers
+  // these are all numbers from 100 -> 999
+  for(int i=100; i<1000; i++){
+    for(int j=100; j<1000; j++){
+      result = i * j;
+      if(is_palindromic_number(result)){
+        if(result > largest){
+          largest = result;
+        }
+      }
     }
   }
-  std::cout << sum << std::endl; // 233,168
+
+  std::cout << largest << std::endl;  // 906609
   return 0;
+}
+
+
+bool is_palindromic_number(long int x){
+  // returns true if x is a palindromic number, false otherwise
+  std::string strx = std::to_string(x);
+  std::string strxrev(strx);
+  std::reverse(strxrev.begin(), strxrev.end());
+  if(strx.compare(strxrev) == 0){
+    return true;
+  }else{
+    return false;
+  }
 }
