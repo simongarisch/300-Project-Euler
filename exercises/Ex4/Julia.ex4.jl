07e0b54f4b14e2d0e51af8e541951edcc2da5367
simongarisch <gatman946@gmail.com>
2019-01-06 21:08:47 +1100

    Adding Julia example for Ex4.

--- exercises/Ex4/Julia.ex4.jl
+++ exercises/Ex4/Julia.ex4.jl
@@ -1,15 +1,33 @@
 #=
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A palindromic number reads the same both ways. The largest palindrome made from
+the product of two 2-digit numbers is 9009 = 91 * 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
 =#
 
-sum = 0
-for i = 1:999
-    if i % 3 == 0 || i % 5 == 0
-        global sum
-        sum += i
+function is_palindromic_number(x)
+  # returns true if x is a palindromic number, false otherwise
+  strx = string(x)
+  if strx == reverse(strx)
+      return true
+  else
+      return false
+  end
+end
+
+
+largest = 0;
+# go through pairs of three digit numbers
+# these are all numbers from 100 -> 999
+for i in 100:999
+    for j in 100:999
+        result = i * j
+        global largest
+        if is_palindromic_number(result)
+            if result > largest
+                largest = result
+            end
+        end
     end
 end
 
-println(sum) # 233,168
+println(largest)  # 906609
