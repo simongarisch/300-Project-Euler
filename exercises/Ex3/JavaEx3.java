40b75f593561f6d684df93cdb8a6c090fb17e8a5
simongarisch <gatman946@gmail.com>
2019-01-06 20:36:46 +1100

    Adding the Java example for Ex3

--- exercises/Ex3/JavaEx3.java
+++ exercises/Ex3/JavaEx3.java
@@ -1,19 +1,36 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
 */
 
-public class JavaEx1 {
+public class JavaEx3 {
 
-    public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
-          }
+  public static void main(String[] args){
+    long largest = 0;             // the largest prime factor
+    long target = 600851475143L;  // for a target number
+
+    for(long i=2; i<Math.pow(target, 0.5); i++){
+      if(target % i == 0){  // it's a factor
+        if(is_prime(i)){    // and it's a prime
+          largest = i;
         }
-        System.out.println(sum); // 233,168
+      }
     }
+
+    System.out.println(largest);  // 6,857
+  }
+
+  public static boolean is_prime(long x){
+    // will return true if x is prime, false otherwise
+    if(x == 2 || x == 3){
+      return true;
+    }
+    for(long i=2; i<x; i++){
+      if(x % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
 
 }
