98916043607f0ba11a2b4e624897405c70ac62d6
simongarisch <gatman946@gmail.com>
2019-01-18 17:40:48 +1100

    Adding R ex14

--- exercises/Ex014/R.ex14.R
+++ exercises/Ex014/R.ex14.R
@@ -1,13 +1,42 @@
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# The following iterative sequence is defined for the set of positive integers:
+#
+# n: n/2 (n is even)
+# n: 3n + 1 (n is odd)
+#
+# Using the rule above and starting with 13, we generate the following sequence:
+#
+# 13  40  20  10  5  16 8  4  2  1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+# Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+#
+# Which starting number, under one million, produces the longest chain?
+#
+# NOTE: Once the chain starts the terms are allowed to go above one million.
 
-sum <- 0
-for(i in 1:999){
-  if((i %% 3 == 0) || (i %% 5 == 0)){
-    sum <- sum + i
+FINISH <- 1000000
+
+longest_chain = 0
+starting <- 0
+
+for(i in 1:(FINISH-1)){
+  terms <- 1
+  num <- i
+
+  while(num != 1){
+    is_even <- (num %% 2 == 0)
+    if(is_even){
+      num <- num / 2
+    }else{
+      num <- 3 * num + 1
+    }
+    terms = terms + 1
+  }
+
+  if(terms > longest_chain){
+    longest_chain <- terms
+    starting <- i
   }
 }
 
-print(sum) # 233,168
+print(starting)  # 837,799
