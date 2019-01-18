9071eb885f522be3656196a4c11c3eca512f6352
simongarisch <gatman946@gmail.com>
2019-01-18 17:32:02 +1100

    Adding Python ex14

--- exercises/Ex014/Python.ex14.py
+++ exercises/Ex014/Python.ex14.py
@@ -1,12 +1,12 @@
 '''
 The following iterative sequence is defined for the set of positive integers:
 
-n → n/2 (n is even)
-n → 3n + 1 (n is odd)
+n: n/2 (n is even)
+n: 3n + 1 (n is odd)
 
 Using the rule above and starting with 13, we generate the following sequence:
 
-13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+13  40  20  10  5  16 8  4  2  1
 It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
 Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 
@@ -15,9 +15,19 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 '''
 
-sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
+FINISH = 1000000
 
-print(sum) # 233,168
+longest_chain = 0
+starting = 0
+for i in range(1, FINISH):
+    terms = 1
+    num = i
+    while num != 1:
+        is_even = (num % 2 == 0)
+        num = (num / 2) if is_even else (3 * num + 1)
+        terms += 1
+    if terms > longest_chain:
+        longest_chain = terms
+        starting = i
+
+print(starting)  # 837,799
