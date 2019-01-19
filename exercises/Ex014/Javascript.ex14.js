42565060440ed31cd7efd47d92841b06694f4ea4
simongarisch <gatman946@gmail.com>
2019-01-20 10:14:10 +1100

    Adding JS ex14.

--- exercises/Ex014/Javascript.ex14.js
+++ exercises/Ex014/Javascript.ex14.js
@@ -1,14 +1,39 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The following iterative sequence is defined for the set of positive integers:
+
+n: n/2 (n is even)
+n: 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13  40  20  10  5  16 8  4  2  1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
-  }
+var FINISH = 1000000;
+
+var longest_chain = 0;
+var starting = 0;
+for(var i=1; i<FINISH; i++){
+    var terms = 1;
+    var num = i;
+    while(num != 1){
+        if(num % 2 == 0){
+            num = num / 2;
+        }else{
+            num = 3 * num + 1;
+        }
+        terms += 1;
+    }
+    if(terms > longest_chain){
+        longest_chain = terms;
+        starting = i;
+    }
 }
 
-console.log(sum); // 233,168
+console.log(starting)  // 837,799
