3566018150b7a7593452a5600bed902e49102544
simongarisch <gatman946@gmail.com>
2019-01-20 10:20:37 +1100

    Adding Lua ex14.

--- exercises/Ex014/Lua.ex14.lua
+++ exercises/Ex014/Lua.ex14.lua
@@ -1,14 +1,39 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The following iterative sequence is defined for the set of positive integers:
+
+n: n/2 (n is even)
+n: 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13  40  20  10  5  16 8  4  2  1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
-  end
+FINISH = 1000000
+
+longest_chain = 0
+starting = 0
+for i = 1, (FINISH-1) do
+    terms = 1
+    num = i
+    while num ~= 1 do
+        if num % 2 == 0 then
+            num = num / 2
+        else
+            num = 3 * num + 1
+        end
+        terms = terms + 1
+    end
+    if terms > longest_chain then
+        longest_chain = terms
+        starting = i
+    end
 end
 
-print(sum) -- 233,168
+print(starting)  -- 837,799
