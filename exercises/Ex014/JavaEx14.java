06c0065ebad893337b8b1f5abbbbd5dbe6f3e528
simongarisch <gatman946@gmail.com>
2019-01-20 10:49:29 +1100

    Adding C# ex14.

--- exercises/Ex014/JavaEx14.java
+++ exercises/Ex014/JavaEx14.java
@@ -1,10 +1,21 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The following iterative sequence is defined for the set of positive integers:
+
+n: n/2 (n is even)
+n: 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13  40  20  10  5  16 8  4  2  1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
 */
 
-public class JavaEx1 {
+public class JavaEx14 {
 
     public static void main(String[] args) {
         int sum = 0;
