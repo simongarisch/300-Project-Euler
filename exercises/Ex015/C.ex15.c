7b2655efacfe02d45763c3f228665aa1e2bf9e70
simongarisch <gatman946@gmail.com>
2019-01-21 01:22:51 +1100

    Working on C Ex15.

--- exercises/Ex015/C.ex15.c
+++ exercises/Ex015/C.ex15.c
@@ -1,18 +1,42 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Starting at the top left corner of a 2 x 2 grid, and only being able to move
+to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20 x 20 grid?
 */
 
 #include <stdio.h>
+#include "BigInt/BigInt.h"
+
+#define SIZE 20
+#define ONE BigInt_construct(1);
+
+BigInt* factorial(int n);
+BigInt* binom(BigInt* n, BigInt* k);
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
-  }
-  printf("%d\n", sum); // 233,168
+  BigInt* size = BigInt_construct(SIZE)
+  BigInt* choices = binom(BigInt_multiply_int(size, 2), size);
+  BigInt_print(choices);  // 137,846,528,820
+
+  BigInt_free(size);
+  BigInt_free(choices);
   return 0;
+}
+
+
+BigInt* factorial(BigInt* n){
+  if(BigInt_to_int(n)==0){
+    return ONE;
+  }else{
+    return BigInt_multiply(n, factorial(BigInt_subtract(n, ONE)));
+  }
+}
+
+
+BigInt* binom(BigInt* n, BigInt* k){
+  BigInt* factn = factorial(n);
+  BigInt* factk = factorial(k);
+  BigInt* factorialnk = factorial(n-k);
+  return factorial(n) / (factorial(k) * factorial(n-k));
 }
