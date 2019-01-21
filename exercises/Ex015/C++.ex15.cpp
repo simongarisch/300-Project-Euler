9c152bddcc362f9773cae363f8db7987bd86b67c
simongarisch <gatman946@gmail.com>
2019-01-21 18:07:51 +1100

    Adding C and C++ ex15

--- exercises/Ex015/C++.ex15.cpp
+++ exercises/Ex015/C++.ex15.cpp
@@ -1,18 +1,54 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Starting at the top left corner of a 2 x 2 grid, and only being able to move
+to the right and down, there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20 x 20 grid?
+
+g++ C++.ex15.cpp -lgmp
 */
 
 #include <iostream>
+#include <gmp.h>
+
+#define SIZE 20
+
+void factorial(long int n, mpz_t* result);
+void init_bigint(mpz_t* b);
+
+using namespace std;
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
-  }
-  std::cout << sum << std::endl; // 233,168
+  long int size = SIZE;
+
+  mpz_t factn, factk, result;
+  init_bigint(&factn);
+  init_bigint(&factk);
+  init_bigint(&result);
+
+  factorial(2*size, &factn);
+  factorial(size, &factk);
+
+  mpz_mul(result, factk, factk);
+  mpz_cdiv_q(result, factn, result);
+  gmp_printf("%Zd\n", result);  // 137,846,528,820
+
+  mpz_clear(factn); // free the space
+  mpz_clear(factk);
+  mpz_clear(result);
   return 0;
+}
+
+
+void factorial(long int n, mpz_t* result){
+  // calculates the factorial of n and stores it in result
+  while(n > 1){
+    // mpz_mul_si (mpz_t rop, const mpz_t op1, long int op2)
+    mpz_mul_si(*result, *result, n);
+    n--;
+  }
+}
+
+
+void init_bigint(mpz_t* b){
+  mpz_init2(*b, 100); // give it 100 bits of space
+  mpz_set_ui(*b, 1);  // and initialize it to one
 }
