53cebd167ae2212e6079f7647c6a86cd52e5c5d8
simongarisch <gatman946@gmail.com>
2019-01-22 09:26:21 +1100

    Adding R ex16

--- exercises/Ex016/R.ex16.R
+++ exercises/Ex016/R.ex16.R
@@ -2,14 +2,17 @@
 # 2^15= 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2^1000?
 
+# use the gmp package
+# https://cran.r-project.org/web/packages/gmp/index.html
+library(gmp)
+
 sum <- 0
-num <- 2 ^ 1000
+num <- as.bigz(2 ^ 1000)
 options(scipen = 999) # avoid scientific notation for this problem
 numstr <- as.character(num)
 numstr_split <- strsplit(numstr, "")[[1]]
 for(char in numstr_split){
-  print(as.numeric(char))
   sum <- sum + as.numeric(char)
 }
 
-print(sum)
+print(sum)  # 1,366
