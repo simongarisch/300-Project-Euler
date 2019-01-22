ba7a5b9c04e35f3af2627a61622af60f8fb5adce
simongarisch <gatman946@gmail.com>
2019-01-22 18:46:52 +1100

    Adding C and C++ ex16

--- exercises/Ex016/C++.ex16.cpp
+++ exercises/Ex016/C++.ex16.cpp
@@ -1,18 +1,56 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+2^15= 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+g++ C++.ex16.cpp -lgmp
 */
 
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <gmp.h>
+
+void init_bigint(mpz_t* b, int start);
+void substring(char origstr[], char substr[], int pos, int length);
+
+using namespace std;
 
 int main(){
+  // get the number we are after
+  mpz_t num;
+  init_bigint(&num, 2);
+  mpz_pow_ui(num, num, 1000);
+  //gmp_printf("%Zd\n", num);
+
+  // convert this to a string
+  char* numstr;
+  mpz_get_str(numstr, 10, num);
+
+  // and get the sum of each digit
   int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
+  char substr[2];
+  for(int i=0; i<strlen(numstr); i++){
+    substring(numstr, substr, i, 1);
+    sum = sum + atoi(substr);
   }
-  std::cout << sum << std::endl; // 233,168
+
+  cout << sum << endl;  // 1,366
   return 0;
+}
+
+
+void init_bigint(mpz_t* b, int start){
+  mpz_init2(*b, 100); // give it 100 bits of space
+  mpz_set_ui(*b, start);
+}
+
+
+void substring(char origstr[], char substr[], int pos, int length){
+  int count = 0;
+  while (count < length) {
+    substr[count] = origstr[pos+count];
+    count++;
+  }
+  substr[count] = '\0';
 }
--- exercises/Ex016/C.ex16.c
+++ exercises/Ex016/C.ex16.c
@@ -1,18 +1,55 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+2^15= 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+gcc C.ex16.c -lgmp
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+void init_bigint(mpz_t* b, int start);
+void substring(char origstr[], char substr[], int pos, int length);
 
 int main(){
+  // get the number we are after
+  mpz_t num;
+  init_bigint(&num, 2);
+  mpz_pow_ui(num, num, 1000);
+  //gmp_printf("%Zd\n", num);
+
+  // convert this to a string
+  char* numstr;
+  mpz_get_str(numstr, 10, num);
+  //printf("%s\n", numstr);
+
+  // and get the sum of each digit
   int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
+  char substr[2];
+  for(int i=0; i<strlen(numstr); i++){
+    substring(numstr, substr, i, 1);
+    sum = sum + atoi(substr);
   }
-  printf("%d\n", sum); // 233,168
+
+  printf("%d\n", sum);  // 1,366
   return 0;
+}
+
+
+void init_bigint(mpz_t* b, int start){
+  mpz_init2(*b, 100); // give it 100 bits of space
+  mpz_set_ui(*b, start);
+}
+
+
+void substring(char origstr[], char substr[], int pos, int length){
+  int count = 0;
+  while (count < length) {
+    substr[count] = origstr[pos+count];
+    count++;
+  }
+  substr[count] = '\0';
 }
