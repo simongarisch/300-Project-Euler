3666123809ea1fe820799147d53c5e392cfa8e80
simongarisch <gatman946@gmail.com>
2019-01-23 12:45:40 +1100

    Adding Lua ex16

--- exercises/Ex016/Lua.ex16.lua
+++ exercises/Ex016/Lua.ex16.lua
@@ -2,11 +2,18 @@
 2^15= 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 What is the sum of the digits of the number 2^1000?
---]]
 
---C:\Downloads\ZeroBraneStudio\bin\lua.exe
---print(_VERSION)  -- Lua 5.1
-require "bc"
+We'll need to use a bigint lib, so install this from luarocks
+luarocks-3.0.4-win32.zip (legacy Windows package, includes Lua 5.1)
+Run the install script with install /L to install the interpreter
+https://github.com/luarocks/luarocks/wiki/Installation-instructions-for-Windows
+http://leafo.net/guides/customizing-the-luarocks-tree.html
+
+We can then install a rock in the current directory\lua_modules with
+luarocks install --tree lua_modules bigint
+
+--]]
+bigint = require("bigint")
 
 function split(s, delimiter)
     result = {};
@@ -17,11 +24,14 @@ function split(s, delimiter)
 end
 
 sum = 0
-num = 2 ^ 1000
-print(num)
-numstr = string.format("%f", num)
-print(numstr)
---for char in numstr:
---    sum += int(char)
+base = bigint.new("2")
+exp = bigint.new("1000")
+num = bigint.exponentiate(base, exp)
+numstr = bigint.unserialize(num, "string")
+--print(numstr)
+for i = 1, #numstr do
+    c = numstr:sub(i, i)
+    sum = sum + tonumber(c)
+end
 
 print(sum)  -- 1,366
