0c64e101444fcf4e7ecbafcccf2eaf855bb4c3ea
simongarisch <gatman946@gmail.com>
2019-01-25 11:09:46 +1100

    Starting Lua ex17

--- exercises/Ex017/Lua.ex17.lua
+++ exercises/Ex017/Lua.ex17.lua
@@ -1,14 +1,41 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If the numbers 1 to 5 are written out in words=> one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+The use of "and" when writing out numbers is in compliance with British usage.
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
-  end
-end
-
-print(sum) -- 233,168
+num2words = {[1]="One",
+             [2]="Two",
+             [3]="Three",
+             [4]="Four",
+             [5]="Five",
+             [6]="Six",
+             [7]="Seven",
+             [8]="Eight",
+             [9]="Nine",
+             [10]="Ten",
+             [11]="Eleven",
+             [12]="Twelve",
+             [13]="Thirteen",
+             [14]="Fourteen",
+             [15]="Fifteen",
+             [16]="Sixteen",
+             [17]="Seventeen",
+             [18]="Eighteen",
+             [19]="Nineteen",
+             [20]="Twenty",
+             [30]="Thirty",
+             [40]="Forty",
+             [50]="Fifty",
+             [60]="Sixty",
+             [70]="Seventy",
+             [80]="Eighty",
+             [90]="Ninety"
+}
+--for k, v in pairs(num2words) do
+--  print(k, v)
+--end
