6e6666640d32860bbfd49ae9993cbccacacba041
simongarisch <gatman946@gmail.com>
2019-01-26 06:29:43 +1100

    Working o JS ex17

--- exercises/Ex017/Javascript.ex17.js
+++ exercises/Ex017/Javascript.ex17.js
@@ -1,14 +1,85 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+The use of "and" when writing out numbers is in compliance with British usage.
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+var num2words =  {"1": "One",
+                 "2": "Two",
+                 "3": "Three",
+                 "4": "Four",
+                 "5": "Five",
+                 "6": "Six",
+                 "7": "Seven",
+                 "8": "Eight",
+                 "9": "Nine",
+                 "10": "Ten",
+                 "11": "Eleven",
+                 "12": "Twelve",
+                 "13": "Thirteen",
+                 "14": "Fourteen",
+                 "15": "Fifteen",
+                 "16": "Sixteen",
+                 "17": "Seventeen",
+                 "18": "Eighteen",
+                 "19": "Nineteen",
+                 "20": "Twenty",
+                 "30": "Thirty",
+                 "40": "Forty",
+                 "50": "Fifty",
+                 "60": "Sixty",
+                 "70": "Seventy",
+                 "80": "Eighty",
+                 "90": "Ninety"
+};
+//console.log(num2words);
+
+
+function get_number_text(num) {
+  // returns the text representing a given integer (up to 1000)
+  var numstr = num.toString();
+  var txt = "";
+
+  if(numstr.length == 4){  // get the thousands
+    txt += num2words[numstr[0]];
+    txt += "Thousand";
+    numstr = numstr.substring(1, numstr.length);
+    if(numstr == "000"){
+      return txt;
+    }
   }
+
+  if(numstr.length == 3){
+    txt += num2words[numstr[0]];
+    txt += "Hundred";
+    numstr = numstr.substring(1, numstr.length);
+    if(numstr == "00"){
+      return txt;
+    }
+  }
+
+  if(txt.length > 0){
+    txt += "And";
+  }
+
+  if(numstr.length == 2){
+    first_digit = parseInt(numstr[0]);
+    second_digit = parseInt(numstr[1]);
+  }
+
+  return "xxx";
 }
 
-console.log(sum); // 233,168
+console.log(get_number_text(1000));
+/*
+var num_chars = 0;
+for(var num=1; num<=1000; num++){
+  var txt = get_number_text(num);
+  num_chars += txt.length;
+}
+console.log(num_chars);
+*/
