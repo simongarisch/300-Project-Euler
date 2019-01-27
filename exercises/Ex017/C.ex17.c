557daa771ca11ca19c4da78ec4bb2d623e0c9cc2
simongarisch <gatman946@gmail.com>
2019-01-27 11:48:40 +1100

    Working on C ex17

--- exercises/Ex017/C.ex17.c
+++ exercises/Ex017/C.ex17.c
@@ -1,18 +1,32 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+The use of "and" when writing out numbers is in compliance with British usage.
 */
 
 #include <stdio.h>
+#include <string.h>
+
+
+struct Numbers{
+   char str[4];
+   char name[10];
+};
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
-  }
-  printf("%d\n", sum); // 233,168
+
+  struct Numbers numbers[2] = {
+    {"1", "One"},
+    {"2", "Two"}
+  };
+
+  printf("%s\n", numbers[0].str);
+  printf("%s\n", numbers[0].name);
+
   return 0;
 }
