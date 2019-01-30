5bf9742a96ae5e94ac3a2e7a21982f8d74254576
simongarisch <gatman946@gmail.com>
2019-01-30 12:17:11 +1100

    Adding description for C# ex17

--- exercises/Ex017/C#.ex17.cs
+++ exercises/Ex017/C#.ex17.cs
@@ -1,7 +1,11 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+The use of "and" when writing out numbers is in compliance with British usage.
 */
 
 using System;
