2538cb9d07725e7e84adb4eadedf5fb2bb11ffe8
simongarisch <gatman946@gmail.com>
2019-02-07 06:53:59 +1100

    Adding Octave ex18

--- exercises/Ex018/Octave.ex18.m
+++ exercises/Ex018/Octave.ex18.m
@@ -1,14 +1,52 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
+it cannot be solved by brute force, and requires a clever method! ;o)
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+dire_path = fileparts(mfilename("fullpath"));
+file_path = strcat(dire_path, "/triangle.txt");
+#exist(file_path);
+#help dlmread
+content = dlmread(file_path, " ");
 
-disp(sum) # 233,168
+# move up the triangle to calculate maximums
+nrows = size(content)(1);
+for irow = (nrows-1):-1:1
+  row = content(irow,:);
+  nextrow = content(irow+1,:);
+  for icol = 1:irow
+    godown = row(icol) + nextrow(icol);
+    goright = row(icol) + nextrow(icol+1);
+    content(irow, icol) = max(goright, godown);
+  endfor
+endfor
+
+disp(content(1,1))  # 1,074
