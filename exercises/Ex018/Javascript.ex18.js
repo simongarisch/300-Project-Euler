393f4b8eb8e9ab917622e8a90935e3f183e086e9
simongarisch <gatman946@gmail.com>
2019-02-06 17:49:29 +1100

    Working on JS ex18.

--- exercises/Ex018/Javascript.ex18.js
+++ exercises/Ex018/Javascript.ex18.js
@@ -1,14 +1,46 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
+it cannot be solved by brute force, and requires a clever method! ;o)
 */
+var path = require("path");
+var fs = require("fs");
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
-  }
+var dire_path = path.resolve(__dirname);
+var file_path = path.join(dire_path, "triangle.txt");
+console.log(file_path);
+
+try {
+  var data = fs.readFileSync(file_path, "utf8");
+  data = data.toString();
+} catch(e) {
+    console.log("Error: ", e.stack);
 }
-
-console.log(sum); // 233,168
+console.log(data);
