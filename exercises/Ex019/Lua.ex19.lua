6b7b888ee53e08a9ede53d04a2c71ce65da1a6fd
simongarisch <gatman946@gmail.com>
2019-02-09 17:40:10 +1100

    Working on Lua ex19

--- exercises/Ex019/Lua.ex19.lua
+++ exercises/Ex019/Lua.ex19.lua
@@ -1,14 +1,19 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
-  end
-end
+count_sundays = 0
+start_date = os.time{year=1901, month=1, day=1}
+end_date = os.time{year=2000, month=12, day=31}
 
-print(sum) -- 233,168
+print(count_sundays)
