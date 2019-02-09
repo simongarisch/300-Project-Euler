0ec7e9843892c1f0729bca6ecc569d6160a5c9db
simongarisch <gatman946@gmail.com>
2019-02-10 08:27:38 +1100

    Adding Lua Ex19

--- exercises/Ex019/Lua.ex19.lua
+++ exercises/Ex019/Lua.ex19.lua
@@ -12,8 +12,38 @@ A leap year occurs on any year evenly divisible by 4, but not on a century unles
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 --]]
 
-count_sundays = 0
-start_date = os.time{year=1901, month=1, day=1}
-end_date = os.time{year=2000, month=12, day=31}
+function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local tbl = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(tbl, str)
+  end
+  return tbl
+end
 
-print(count_sundays)
+exec_file_path = arg[0]  -- path of the current executing file
+tbl = split(exec_file_path, "\\")
+tbl[#tbl] = "date.lua"
+file_path = table.concat(tbl, "\\")
+print(file_path)
+
+-- http://tieske.github.io/date/
+date = dofile(file_path)
+
+count_sundays = 0
+start_date = date("jan 1 1901")
+end_date = date("dec 31 2000")
+
+while start_date <= end_date do
+  if start_date:getday() == 1 then
+    -- getweekday(): The starting day of week (1 for sunday, 2 for monday, ... 7 for saturday).
+    if start_date:getweekday() == 1 then
+      count_sundays = count_sundays + 1
+    end
+  end
+  start_date:adddays(1)
+end
+
+print(count_sundays)  --171
