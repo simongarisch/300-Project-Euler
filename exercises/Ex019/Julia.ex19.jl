6f6d560cc3cdd004145b5a5f455e9c10d807d7ab
simongarisch <gatman946@gmail.com>
2019-02-09 17:27:32 +1100

    Adding Julia ex19

--- exercises/Ex019/Julia.ex19.jl
+++ exercises/Ex019/Julia.ex19.jl
@@ -1,15 +1,30 @@
 #=
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-=#
+You are given the following information, but you may prefer to do some research for yourself.
 
-sum = 0
-for i = 1:999
-    if i % 3 == 0 || i % 5 == 0
-        global sum
-        sum += i
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+=#
+using Dates
+
+count_sundays = 0
+start_date = DateTime(1901,1,1)
+end_date = DateTime(2000,12,31)
+
+while start_date <= end_date
+    global start_date, count_sundays
+    if Dates.day(start_date) == 1
+        if Dates.dayname(start_date) == "Sunday"
+            count_sundays += 1
+        end
     end
+    start_date += Dates.Day(1)
 end
 
-println(sum) # 233,168
+println(count_sundays)  # 171
