7b54db8c09c8117b86ee7f148d85f4fafae6304e
simongarisch <gatman946@gmail.com>
2019-02-09 17:36:59 +1100

    Adding JS ex19

--- exercises/Ex019/Javascript.ex19.js
+++ exercises/Ex019/Javascript.ex19.js
@@ -1,14 +1,28 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+var count_sundays = 0;
+var start_date = new Date("1901-1-1");
+var end_date = new Date("2000-12-31");
+
+while(start_date <= end_date){
+  if(start_date.getDate() == 1){  // first of the month
+    if(start_date.getDay() == 0){   // 0-6 starting on Sunday
+      count_sundays++;
+    }
   }
+  start_date.setDate(start_date.getDate() + 1);
 }
 
-console.log(sum); // 233,168
+console.log(count_sundays);  // 171
