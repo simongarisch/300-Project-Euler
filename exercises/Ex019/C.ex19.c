6b2de1d1690e68076bed30c000df3cd317839066
simongarisch <gatman946@gmail.com>
2019-02-10 13:13:59 +1100

    Adding C and C++ ex19

--- exercises/Ex019/C.ex19.c
+++ exercises/Ex019/C.ex19.c
@@ -1,18 +1,51 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+You are given the following information, but you may prefer to do some research for yourself.
 
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+*/
+#define _XOPEN_SOURCE 700  // otherwise it grumbles about strptime
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  int count_sundays = 0;
+  struct tm tm_start = {0};
+  struct tm tm_end = {0};
+
+  char *s;
+  s = strptime("1 Jan 1901", "%d %b %Y", &tm_start);
+  if (s == NULL) {
+    printf("Cannot parse start date");
+    return 1;
+  }
+
+  s = strptime("31 Dec 2000", "%d %b %Y", &tm_end);
+  if (s == NULL) {
+    printf("Cannot parse end date");
+    return 1;
+  }
+
+  time_t start, end;
+  double diff = 1;
+  while(diff != 0){
+    tm_start.tm_mday += 1;
+    start = mktime(&tm_start);
+    end = mktime(&tm_end);
+    diff = difftime(start, end);
+    if(tm_start.tm_wday == 0){    // we are on a Sunday
+      if(tm_start.tm_mday == 1){  // first of the month
+        count_sundays++;
+      }
     }
   }
-  printf("%d\n", sum); // 233,168
-  return 0;
+
+  printf("%d\n", count_sundays);  // 171
 }
