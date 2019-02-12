e56f5351db68a8d7cd817893b4ae9ae2c6a13e33
simongarisch <gatman946@gmail.com>
2019-02-12 11:53:37 +1100

    Adding Octave ex20

--- exercises/Ex020/Octave.ex20.m
+++ exercises/Ex020/Octave.ex20.m
@@ -5,19 +5,26 @@ For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
+
+In order to implement big int in Octave we've used
+https://au.mathworks.com/matlabcentral/fileexchange/22725-variable-precision-integer-arithmeticss
 %}
 
+vpi_path = strcat(fileparts(mfilename('fullpath')), "/VariablePrecisionIntegers")
+addpath(genpath(vpi_path))
 sum = 0;
-num = 1;
+num = vpi(1);
 for i=1:100
   num *= i;
 end
 
-numstr = num2str(num);
+numstr = strtrim(num2str(num));
 
 for i = 1:length(numstr)
-  sum += str2num(substr(numstr, i, 1));
+  column = str2num(substr(numstr, i, 1));
+  for j = 1:length(column)
+    sum += column(j);
+  end
 end
 
-#save myfile.mat numstr
-disp(sum)
+disp(sum)  # 648
