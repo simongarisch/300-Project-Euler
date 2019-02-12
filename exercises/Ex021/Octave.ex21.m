4b66d63092cb66e453cdf213e15c40d2a36c623e
simongarisch <gatman946@gmail.com>
2019-02-12 22:25:31 +1100

    Adding Octave ex21

--- exercises/Ex021/Octave.ex21.m
+++ exercises/Ex021/Octave.ex21.m
@@ -1,14 +1,48 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
+function sum = sum_divisors(x)
+  # returns the sum for all divisors of x
+  sum = 0;
+  if x > 1
+    for i = 1:(x-1)
+      if mod(x, i) == 0
+        sum += i;
+      end
+    end
   end
 end
 
-disp(sum) # 233,168
+
+function isamic = is_amicable(x)
+  # returns true if x is an amicable number, false otherwise
+  sum_divx = sum_divisors(x);
+  if sum_divx == x
+    isamic = false;
+    return;
+  end
+  if sum_divisors(sum_divx) == x
+    isamic = true;
+  else
+    isamic = false;
+  end
+end
+
+
+
+#disp(is_amicable(220))  # true
+sum = 0;
+for x = 1:(10000-1)
+  if is_amicable(x)
+    sum += x;
+  end
+end
+
+disp(sum) # 31,626
