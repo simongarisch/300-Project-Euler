3a029d7458fc688d987fa03ce797329689229b80
simongarisch <gatman946@gmail.com>
2019-02-13 08:25:37 +1100

    Adding Java ex21

--- exercises/Ex021/JavaEx21.java
+++ exercises/Ex021/JavaEx21.java
@@ -8,16 +8,45 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 Evaluate the sum of all the amicable numbers under 10000.
 */
 
-public class JavaEx1 {
+public class JavaEx21 {
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
+      //System.out.println(is_amicable(220));  // true
+      long sum = 0;
+      for(long x=1; x<10000; x++){
+        if(is_amicable(x)){
+          sum += x;
+        }
+      }
+      System.out.println(sum);  // 31,626
+    }
+
+
+    public static long sum_divisors(long x){
+      // returns the sum for all divisors of x
+      long sum = 0;
+      if(x > 1){
+        for(long i=1; i<x; i++){
+          if(x % i == 0){
+            sum = sum + i;
           }
         }
-        System.out.println(sum); // 233,168
+      }
+      return sum;
+    }
+
+
+    public static boolean is_amicable(long x){
+      // returns true if x is an amicable number, false otherwise
+      long sum_divx = sum_divisors(x);
+      if(sum_divx == x){
+        return false;
+      }
+      if(sum_divisors(sum_divx) == x){
+        return true;
+      }else{
+        return false;
+      }
     }
 
 }
