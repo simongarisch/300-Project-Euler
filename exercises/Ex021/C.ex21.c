4b4ef8a025adc91e6b920be490c38f292d7d00a7
simongarisch <gatman946@gmail.com>
2019-02-13 06:42:30 +1100

    Adding C and C++ ex21

--- exercises/Ex021/C.ex21.c
+++ exercises/Ex021/C.ex21.c
@@ -1,18 +1,56 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
 */
 
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
+long sum_divisors(long x);
+int is_amicable(long x);
+
+
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  //printf("%d\n", is_amicable(220));  // 1
+  long sum = 0;
+  for(long x=1; x<10000; x++){
+    if(is_amicable(x)){
+      sum += x;
     }
   }
-  printf("%d\n", sum); // 233,168
+
+  printf("%ld\n", sum);  // 31,626
   return 0;
+}
+
+int is_amicable(long x){
+  long sum_divx = sum_divisors(x);
+  if(sum_divx == x){
+    return FALSE;
+  }
+  if(sum_divisors(sum_divx) == x){
+    return TRUE;
+  }else{
+    return FALSE;
+  }
+}
+
+long sum_divisors(long x){
+  // returns the sum for all divisors of x
+  long sum = 0;
+  if(x > 1){
+    for(long i=1; i<x; i++){
+      if(x % i == 0){
+        sum = sum + i;
+      }
+    }
+  }
+  return sum;
 }
