040b7a708e52c773d1e5a180ec35c31de383ef61
simongarisch <gatman946@gmail.com>
2019-02-12 22:07:15 +1100

    Adding Lua ex21

--- exercises/Ex021/Lua.ex21.lua
+++ exercises/Ex021/Lua.ex21.lua
@@ -1,14 +1,46 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+function sum_divisors(x)
+  -- returns the sum for all divisors of x
+  local sum = 0
+  if x > 1 then
+    for i = 1,(x-1) do
+      if x % i == 0 then
+        sum = sum + i
+      end
+    end
+  end
+  return sum
+end
+
+
+function is_amicable(x)
+  -- returns true if x is an amicable number, false otherwise
+  sum_divx = sum_divisors(x)
+  if sum_divx == x then
+    return false
+  end
+  if sum_divisors(sum_divx) == x then
+    return true
+  else
+    return false
   end
 end
 
-print(sum) -- 233,168
+--print(is_amicable(220)) -- true
+sum = 0
+for x = 1,(10000-1) do
+  if is_amicable(x) then
+    sum = sum + x
+  end
+end
+
+print(sum)  -- 31,626
