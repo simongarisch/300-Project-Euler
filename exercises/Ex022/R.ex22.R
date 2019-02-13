c7395276abd12e9f70951748b989e0f922d740f2
simongarisch <gatman946@gmail.com>
2019-02-13 17:26:18 +1100

    Adding R ex22

--- exercises/Ex022/R.ex22.R
+++ exercises/Ex022/R.ex22.R
@@ -1,13 +1,45 @@
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# Using names.txt (right click and 'Save Link/Target As...'),
+# a 46K text file containing over five-thousand first names,
+# begin by sorting it into alphabetical order.
+#
+# Then working out the alphabetical value for each name,
+# multiply this value by its alphabetical position in the list to obtain a name score.
+#
+# For example, when the list is sorted into alphabetical order,
+# COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+# So, COLIN would obtain a score of 938 * 53 = 49714.
+#
+# What is the total of all the name scores in the file?
 
-sum <- 0
-for(i in 1:999){
-  if((i %% 3 == 0) || (i %% 5 == 0)){
-    sum <- sum + i
+args <- commandArgs(trailingOnly = F)
+dire_path <- normalizePath(dirname(sub("^--file=", "", args[grep("^--file=", args)])))
+file_path <- file.path(dire_path, "names.txt")
+df <- read.table(file_path, sep="\n")
+
+names <- strsplit(as.character(df[1,]), ",")
+names <- list(sort(unlist(names)))
+
+scores_mapping <- 1:length(LETTERS)
+names(scores_mapping) <- LETTERS
+#print(scores_mapping)
+
+get_name_score <- function(name){
+  name_score <- 0
+  name_split <- strsplit(name, "")[[1]]
+  for(char in name_split){
+    name_score = name_score + scores_mapping[char][[1]]
   }
+  return(name_score)
+}
+#print(get_name_score("COLIN"))  # 53
+
+total_score = 0
+counter = 0
+for(name in names[[1]]){
+   counter = counter + 1
+   name_score = get_name_score(name)
+   total_score = total_score + counter * name_score
 }
 
-print(sum) # 233,168
+print(total_score)  # 871,198,282
