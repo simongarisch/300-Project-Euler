ca96bc378b7eb4fd06ff8925ed6e288d131ff271
simongarisch <gatman946@gmail.com>
2019-02-14 18:41:32 +1100

    Adding Lua ex22

--- exercises/Ex022/Lua.ex22.lua
+++ exercises/Ex022/Lua.ex22.lua
@@ -1,14 +1,82 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order.
+
+Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order,
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 * 53 = 49714.
+
+What is the total of all the name scores in the file?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
   end
+  local tbl = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(tbl, str)
+  end
+  return tbl
 end
 
-print(sum) -- 233,168
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+
+function read_file(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+
+exec_file_path = arg[0]  -- path of the current executing file
+tbl = split(exec_file_path, "\\")
+tbl[#tbl] = "names.txt"
+file_path = table.concat(tbl, "\\")
+--print(file_path)
+
+names = split(read_file(file_path)[1]:gsub('"', ""), ",")
+table.sort(names)
+--for k,v in pairs(names) do print(k,v) end
+
+alphabet = string.upper("a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z")
+alphabet = split(alphabet, ".")
+--for k,v in pairs(alphabet) do print(k,v) end
+
+function get_name_score(name)
+  local name_score = 0
+  local char
+  for i=1, string.len(name) do
+    -- get the character for lookup
+    char = string.sub(name, i, i)
+    -- and search for this in our alphabet table
+    for k,v in pairs(alphabet) do
+      if v == char then
+        name_score = name_score + k
+        break
+      end
+    end
+  end
+  return name_score
+end
+--print(get_name_score("COLIN"))  -- 53
+
+total_score = 0
+for key,name in pairs(names) do
+  name_score = get_name_score(name)
+  total_score = total_score + (key * name_score)
+end
+print(total_score)  -- 871,198,282
