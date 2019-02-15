0b9dc71e3ebd69a775e0fe46b9938931d2460aa2
simongarisch <gatman946@gmail.com>
2019-02-15 11:56:44 +1100

    Adding C and C++ ex22

--- exercises/Ex022/C++.ex22.cpp
+++ exercises/Ex022/C++.ex22.cpp
@@ -1,18 +1,123 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order.
+
+Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order,
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 * 53 = 49714.
+
+What is the total of all the name scores in the file?
 */
 
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#define MAXCHAR 50000
+
+using namespace std;
+
+int file_exists(char *file_path);
+void read_file(char *file_path, char *contents);
+int get_name_score(char *name);
+
+int compare_function(const void *x, const void *y){
+  return(strcmp(*((char**)x),*((char**)y)));
+}
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  char *file_name = (char *)"names.txt";
+  char contents[MAXCHAR];
+  char contentscopy[MAXCHAR];
+  read_file(file_name, contents);
+
+  // find how many names we want to store
+  strcpy(contentscopy, contents);
+  long namescount = 0;
+  char *pch = strtok(contentscopy, ",");
+  while (pch != NULL){
+    namescount++;
+    pch = strtok (NULL, ",");
+  }
+
+  // and collect these
+  char **names = (char **)malloc(namescount * sizeof(char *));
+  strcpy(contentscopy, contents);
+  namescount = 0;
+  pch = strtok(contentscopy, ",");
+  while (pch != NULL){
+    names[namescount] = (char *)malloc(sizeof(pch) + 1);
+    strcpy(names[namescount], pch);
+    namescount++;
+    pch = strtok (NULL, ",");
+  }
+
+  // sort these names
+  qsort(names, namescount, sizeof(char *), compare_function);
+
+  long total_score=0;
+  char *name;
+  int name_score;
+  for(int i=0; i<namescount; i++){
+    name = names[i];
+    name_score = get_name_score(name);
+    total_score = total_score + name_score * (i + 1);
+  }
+  cout << total_score << endl;  // 871,198,282
+
+  // free our array
+  for (int i=0; i<namescount; i++){
+    free(names[i]);
+  }
+  free(names);
+
+  return 0;
+}
+
+
+int get_name_score(char *name){
+  char *alphabet = (char *)"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int name_score = 0;
+  for(int i=0; i<strlen(name); i++){
+    for(int j=0; j<strlen(alphabet); j++){
+      if(name[i] == alphabet[j]){
+        name_score += (j + 1);
+        break;
+      }
     }
   }
-  std::cout << sum << std::endl; // 233,168
+  return name_score;
+}
+
+
+int file_exists(char *file_path){
+  FILE *file;
+  if ((file = fopen(file_path, "r"))){
+    fclose(file);
+    return 1;
+  }
   return 0;
+}
+
+
+void read_file(char *file_path, char *contents){
+  FILE *fp;
+  char str[MAXCHAR];
+  strcpy(contents, "");  // initialize as blank
+
+  fp = fopen(file_path, "r");
+  if (fp == NULL){
+    printf("Could not open file %s", file_path);
+    return;
+  }
+
+  while (fgets(str, MAXCHAR, fp) != NULL){
+    strcat(contents, str);
+  }
+  fclose(fp);
 }
--- exercises/Ex022/C.ex22.c
+++ exercises/Ex022/C.ex22.c
@@ -1,18 +1,133 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order.
+
+Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order,
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 * 53 = 49714.
+
+What is the total of all the name scores in the file?
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXCHAR 50000
+
+int file_exists(char *file_path);
+void read_file(char *file_path, char *contents);
+int get_name_score(char *name);
+
+int compare_function(const void *x, const void *y){
+  return(strcmp(*((char**)x),*((char**)y)));
+}
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  char *file_name = "names.txt";
+  char contents[MAXCHAR];
+  char contentscopy[MAXCHAR];
+  read_file(file_name, contents);
+  //printf("%s\n", contents);
+
+  // find how many names we want to store
+  strcpy(contentscopy, contents);
+  long namescount = 0;
+  char *pch = strtok(contentscopy, ",");
+  while (pch != NULL){
+    //printf("%s\n", pch);
+    namescount++;
+    pch = strtok (NULL, ",");
+  }
+
+  // and collect these
+  char **names = (char **)malloc(namescount * sizeof(char *));
+  strcpy(contentscopy, contents);
+  namescount = 0;
+  pch = strtok(contentscopy, ",");
+  while (pch != NULL){
+    names[namescount] = (char *)malloc(sizeof(pch) + 1);
+    strcpy(names[namescount], pch);
+    namescount++;
+    pch = strtok (NULL, ",");
+  }
+
+  // sort these names
+  qsort(names, namescount, sizeof(char *), compare_function);
+
+  // check we have the names in alhabetical order
+  //for(int i=0; names[i]; i++){
+  //  printf("%s\n", names[i]);
+  //}
+
+  //printf("%d\n", get_name_score("COLIN"));  //53
+  long total_score=0;
+  char *name;
+  int name_score;
+  for(int i=0; i<namescount; i++){
+    name = names[i];
+    //printf("%s\n", name);
+    name_score = get_name_score(name);
+    //printf("%d\n", name_score);
+    total_score = total_score + name_score * (i + 1);
+  }
+  printf("%ld\n", total_score);  // 871,198,282
+
+  // free our array
+  for (int i=0; i<namescount; i++){
+    free(names[i]);
+  }
+  free(names);
+
+  return 0;
+}
+
+
+int get_name_score(char *name){
+  char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int name_score = 0;
+  for(int i=0; i<strlen(name); i++){
+    //printf("%c\n", name[i]);
+    for(int j=0; j<strlen(alphabet); j++){
+      if(name[i] == alphabet[j]){
+        name_score += (j + 1);
+        break;
+      }
     }
   }
-  printf("%d\n", sum); // 233,168
+  return name_score;
+}
+
+
+int file_exists(char *file_path){
+  FILE *file;
+  if ((file = fopen(file_path, "r"))){
+    fclose(file);
+    return 1;
+  }
   return 0;
+}
+
+
+void read_file(char *file_path, char *contents){
+  //printf("%d: file exists\n", file_exists(file_path));
+  FILE *fp;
+  char str[MAXCHAR];
+  strcpy(contents, "");  // initialize as blank
+
+  fp = fopen(file_path, "r");
+  if (fp == NULL){
+    printf("Could not open file %s", file_path);
+    return;
+  }
+
+  while (fgets(str, MAXCHAR, fp) != NULL){
+    strcat(contents, str);
+  }
+  fclose(fp);
 }
