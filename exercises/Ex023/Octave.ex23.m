c9253d02b39b86f553ef9b7394d443b2b25bed56
simongarisch <gatman946@gmail.com>
2019-02-17 15:01:31 +1100

    Working on Octave ex23

--- exercises/Ex023/Octave.ex23.m
+++ exercises/Ex023/Octave.ex23.m
@@ -1,14 +1,46 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A perfect number is a number for which the sum of its proper divisors is
+exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be
+1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n
+and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
+the smallest number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than 28123
+can be written as the sum of two abundant numbers. However, this upper limit cannot
+be reduced any further by analysis even though it is known that the greatest number
+that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
+LIMIT = 28123;
+
+function sum_divisors = get_sum_proper_divisors(x)
+  # returns the sum of all proper divisors for x
+  sum_divisors = 0;
+  if x == 1
+    return;
+  end
+  divisors = [];
+  for i = 1:floor(x/2)
+    if mod(x, i) == 0
+      divisors = [divisors ; i];
+    end
+  end
+  #disp(divisors)
+  sum_divisors = sum(divisors);
+end
+#get_sum_proper_divisors(12)  # 16
+
+
+abundant_numbers = [];
+for x = 1:LIMIT
+  if get_sum_proper_divisors(x) > x
+    abundant_numbers = [abundant_numbers ; x];
   end
 end
-
-disp(sum) # 233,168
+length(abundant_numbers)
