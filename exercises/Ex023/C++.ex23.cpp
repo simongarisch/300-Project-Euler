15b5998dbace61b2e51d69b398e8dd99b7edda3b
simongarisch <gatman946@gmail.com>
2019-02-18 14:29:55 +1100

    Adding C and C++ ex23

--- exercises/Ex023/C++.ex23.cpp
+++ exercises/Ex023/C++.ex23.cpp
@@ -1,18 +1,133 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A perfect number is a number for which the sum of its proper divisors is
+exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be
+1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n
+and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
+the smallest number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than 28123
+can be written as the sum of two abundant numbers. However, this upper limit cannot
+be reduced any further by analysis even though it is known that the greatest number
+that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 */
-
 #include <iostream>
+#include <cstdlib>
 
-int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+#define LIMIT 28123
+
+using namespace std;
+
+long get_sum_proper_divisors(long x);
+void get_combinations(long *combinations, long *abundant_numbers, long counter);
+int incombinations(long *combinations, long counter, long x);
+
+int compare_function(const void *px, const void *py){
+  long x = *((long*)px);
+  long y = *((long*)py);
+  return x - y;
+}
+
+
+int main(void){
+  long *abundant_numbers = (long*)calloc(LIMIT, sizeof(long));
+  long counter = 0;
+  for(long x=1; x<=LIMIT; x++){
+    if(get_sum_proper_divisors(x) > x){
+      abundant_numbers[counter] = x;
+      counter++;
     }
   }
-  std::cout << sum << std::endl; // 233,168
-  return 0;
+
+  // create an array of combinations and fill it
+  long combinations_length = counter * counter;
+  long *combinations = (long*)calloc(combinations_length, sizeof(long));
+  get_combinations(combinations, abundant_numbers, counter);
+  qsort(combinations, combinations_length, sizeof(long), compare_function);
+
+  // We have all of the sorted combinations, but there are duplicates...
+  // Create a new set of combinations with no duplicated values.
+  long *uniquecombinations = (long*)calloc(combinations_length, sizeof(long));
+  long unique_counter = 0;
+  long current_value, last = 0;
+  for(long i=0; i<combinations_length; i++){
+    current_value = combinations[i];
+    if(current_value != last){
+      uniquecombinations[unique_counter] = current_value;
+      unique_counter++;
+    }
+    last = current_value;
+  }
+
+  // We have the combinations of two abundant numbers we need
+  // Find all numbers that cannot be expressed as the sum of two of these.
+  long sumc = 0;
+  for(long x=1; x<=LIMIT; x++){
+    if(incombinations(uniquecombinations, counter, x) == true){
+      continue;
+    }else{
+      sumc = sumc + x;
+    }
+  }
+
+  cout << sumc << endl;  // 4,179,871
+  free(abundant_numbers);
+  free(combinations);
+  free(uniquecombinations);
+	return 0;
+}
+
+
+long get_sum_proper_divisors(long x){
+  long sumd = 0;
+  if(x == 1){
+    return 0;
+  }
+  for(long i=1; i<=(x/2); i++){
+    if(x % i == 0){
+      sumd = sumd + i;
+    }
+  }
+  return sumd;
+}
+
+
+void get_combinations(long *combinations, long *abundant_numbers, long counter){
+  // fills the combinations array with pairs of summed abundant_numbers
+  long ccounter = 0;
+  long combinations_length = counter * counter;
+  long numi, numj, sum;
+  int include;
+  for(long i=0; i<counter; i++){
+    for(long j=0; j<counter; j++){
+      // get the sum of these two numbers
+      numi = abundant_numbers[i];
+      numj = abundant_numbers[j];
+      sum = numi + numj;
+      combinations[ccounter] = sum;
+      ccounter++;
+    }
+  }
+}
+
+
+int incombinations(long *combinations, long counter, long x){
+  // checks whether x is in our combinations array
+  for(long k=0; k<(counter*counter); k++){
+    if(combinations[k] == x){
+      // x found... return TRUE and exit
+      return true;
+    }
+    if(combinations[k] == 0){
+      // we hit a zero from orig calloc (so x not found)
+      // return FALSE and exit
+      return false;
+    }
+  }
+  return true;
 }
