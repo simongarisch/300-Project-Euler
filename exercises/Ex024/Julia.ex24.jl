dd4cb87da9ab7eabfea898faed14c9220fbea36c
simongarisch <gatman946@gmail.com>
2019-02-19 06:04:13 +1100

    Adding Julia ex24

--- exercises/Ex024/Julia.ex24.jl
+++ exercises/Ex024/Julia.ex24.jl
@@ -1,15 +1,19 @@
 #=
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A permutation is an ordered arrangement of objects.
+For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+If all of the permutations are listed numerically or alphabetically,
+we call it lexicographic order.
+The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits
+0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 =#
+using Combinatorics
 
-sum = 0
-for i = 1:999
-    if i % 3 == 0 || i % 5 == 0
-        global sum
-        sum += i
-    end
-end
+# https://github.com/JuliaMath/Combinatorics.jl
+# permutations(a): Generate all permutations of an indexable object a in lexicographic order.
 
-println(sum) # 233,168
+numstr = [string(i) for i in 0:9]
+println(parse(Int64, join(collect(permutations(numstr))[Int(1e6)], "")))  # 2,783,915,460
