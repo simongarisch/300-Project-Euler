a1bcb213b5d1b8c0de69655eee96fb806f1c12f9
simongarisch <gatman946@gmail.com>
2019-02-19 16:06:54 +1100

    Adding C# ex24

--- exercises/Ex024/C#.ex24.cs
+++ exercises/Ex024/C#.ex24.cs
@@ -1,19 +1,54 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A permutation is an ordered arrangement of objects.
+For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+If all of the permutations are listed numerically or alphabetically,
+we call it lexicographic order.
+The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits
+0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
 
 using System;
+using System.Collections.Generic;
 
-class Ex1{
+
+class Ex24{
+
+  private static List<String> perms = new List<String>();
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
-      }
-    }
-    Console.WriteLine(sum); // 233,168
+    // get our array from 0 -> 9
+    String numstr = "0123456789";
+    int len = numstr.Length;
+    permgen(numstr, numstr.Length);
+    perms.Sort();
+    Console.WriteLine(long.Parse(perms[1000000-1]));  // 2,783,915,460
   }
+
+  private static void permgen(String numstr, int n){
+      if (n == 0){
+        perms.Add(numstr);
+        //Console.WriteLine(numstr);
+      }else{
+          for(int i=0; i<n; i++){
+            numstr = swap(numstr, i, n-1);
+            permgen(numstr, n-1);
+            numstr = swap(numstr, i, n-1);
+          }
+      }
+  }
+
+  private static String swap(String numstr, int i, int j){
+    char temp;
+    char[] chars = numstr.ToCharArray();
+    temp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = temp;
+    string s = new string(chars);
+    return s;
+  }
+
 }
