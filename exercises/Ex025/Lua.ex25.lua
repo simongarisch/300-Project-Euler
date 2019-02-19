c551e9e17a44c3adae4ca0a354b02833f3f0a6bf
simongarisch <gatman946@gmail.com>
2019-02-20 07:04:26 +1100

    Adding Lua ex25

--- exercises/Ex025/Lua.ex25.lua
+++ exercises/Ex025/Lua.ex25.lua
@@ -1,14 +1,54 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The Fibonacci sequence is defined by the recurrence relation:
+F(n) = F(n-1) + F(n-2), where F(1) = 1 and F(2) = 1
+
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
   end
+  local tbl = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(tbl, str)
+  end
+  return tbl
 end
 
-print(sum) -- 233,168
+exec_file_path = arg[0]  -- path of the current executing file
+tbl = split(exec_file_path, "\\")
+tbl[#tbl] = "bigint.lua"
+file_path = table.concat(tbl, "\\")
+
+bigint = dofile(file_path)
+
+fibindex = 3
+fib1 = bigint.new("1")
+fib2 = bigint.new("1")
+fibvalue = bigint.new("2")
+
+while string.len(bigint.unserialize(fibvalue, "string")) < 1000 do
+  fibindex = fibindex + 1
+  fib1 = fib2
+  fib2 = fibvalue
+  fibvalue = bigint.add(fib1, fib2)
+end
+
+print(fibindex) -- 4,782
