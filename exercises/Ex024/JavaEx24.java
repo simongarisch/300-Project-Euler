bbb86f7e0284a98676f3aee35a9ae5558ae97208
simongarisch <gatman946@gmail.com>
2019-02-19 16:25:36 +1100

    Adding Java ex24

--- exercises/Ex024/JavaEx24.java
+++ exercises/Ex024/JavaEx24.java
@@ -1,19 +1,51 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A permutation is an ordered arrangement of objects.
+For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+If all of the permutations are listed numerically or alphabetically,
+we call it lexicographic order.
+The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits
+0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
+import java.util.*;
 
-public class JavaEx1 {
 
-    public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
-          }
+public class JavaEx24{
+
+    private static List<String> perms = new ArrayList<String>();
+
+    public static void main(String[] args){
+      String numstr = "0123456789";
+      permgen(numstr, numstr.length());
+      Collections.sort(perms);
+      int idx = (int)(Math.pow(10, 6)-1);
+      System.out.println(Long.parseLong(perms.get(idx)));  // 2,783,915,460
+    }
+
+    private static void permgen(String numstr, int n){
+        if (n == 0){
+          perms.add(numstr);
+          //System.out.println(numstr);
+        }else{
+            for(int i=0; i<n; i++){
+              numstr = swap(numstr, i, n-1);
+              permgen(numstr, n-1);
+              numstr = swap(numstr, i, n-1);
+            }
         }
-        System.out.println(sum); // 233,168
+    }
+
+    private static String swap(String numstr, int i, int j){
+      char temp;
+      char[] chars = numstr.toCharArray();
+      temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+      String s = new String(chars);
+      return s;
     }
 
 }
