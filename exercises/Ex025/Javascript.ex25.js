aca3095995f239102288da74015afa048548bcf3
simongarisch <gatman946@gmail.com>
2019-02-20 06:56:02 +1100

    Adding JS ex25

--- exercises/Ex025/Javascript.ex25.js
+++ exercises/Ex025/Javascript.ex25.js
@@ -1,14 +1,38 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+The Fibonacci sequence is defined by the recurrence relation:
+F(n) = F(n-1) + F(n-2), where F(1) = 1 and F(2) = 1
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
-  }
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+*/
+var bigInt = require("big-integer");
+
+var fibindex = 3
+var fib1 = bigInt("1")
+var fib2 = bigInt("1")
+var fibvalue = bigInt("2")
+
+while(fibvalue.toString().length < 1000){
+  fibindex += 1
+  fib1 = fib2
+  fib2 = fibvalue
+  fibvalue = fib1.plus(fib2)
 }
 
-console.log(sum); // 233,168
+
+console.log(fibindex);  // 4,782
