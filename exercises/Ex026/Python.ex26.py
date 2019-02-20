1ef2c94f6236ac183cdb1e7ecab215ac9c335782
simongarisch <gatman946@gmail.com>
2019-02-20 13:19:32 +1100

    Kicking off ex26

--- exercises/Ex026/Python.ex26.py
+++ exercises/Ex026/Python.ex26.py
@@ -1,12 +1,67 @@
 '''
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A unit fraction contains 1 in the numerator.
+The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle.
+It can be seen that 1/7 has a 6-digit recurring cycle.
+
+Find the value of d < 1000 for which 1/d contains the longest
+recurring cycle in its decimal fraction part.
+
+NOTES:
+We'll approach this dividing by some int and keeping track of the result, remainder
+and significant figure we're working with.
+If we divide my a int (i) then the maximum number of potential remainders we can have is (i-1)
+For example, if we have x / 12, possible remainders (if there are any) include
+1, 2, 3, ... 11.
+If we find the same remainder again them the pattern will repeat.
 '''
 
-sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
+longest_pattern = 0   # the longest recurring cycle
+longest_divisor = 0   # for a particular divisor
 
-print(sum) # 233,168
+for i in range(2, 1000):
+    n = 1        # numerator to be divided by
+    result = []  # track the result of our division
+
+    # the maximum number of potential remainders we can have is (i-1)
+    remainders_found = {k: False for k in range(1, i)}
+    pattern_length = 0
+    pattern_found = False
+
+    while(n != 0):
+        n *= 10         # go to the next significant figure
+        wholes = n / i  # get the number of times n is evenly divisible by i
+        n = n % i       # continue with division for the remainder
+        result.append(wholes)
+        if n == 0:
+            break
+
+        # check if we have seen this remainder before
+        if remainders_found[n]:
+            if not pattern_found:
+                idx1 = result.index(wholes)
+                idx2 = len(result)
+                pattern_length = idx2 - idx1 - 1
+                if pattern_length > longest_pattern:
+                    longest_pattern = pattern_length
+                    longest_divisor = i
+                pattern_found = True
+
+        remainders_found[n] = True
+        if pattern_found and len(result) > 10:
+            break
+
+    #decimal = "%0.10f" % float("0." + "".join([str(x) for x in result]))
+    #print(str(i) + "\t" + str(decimal) + "\t" + str(pattern_length))
+
+print(longest_divisor)  # 983
