235450ed1c02b654b419057cd0a24deecf9baedb
simongarisch <gatman946@gmail.com>
2019-02-23 05:31:07 +1100

    Adding Lua ex26

--- exercises/Ex026/Lua.ex26.lua
+++ exercises/Ex026/Lua.ex26.lua
@@ -48,7 +48,7 @@ for i = 2,999 do
     if n == 0 then break end
 
     -- check if we have seen this remainder before
-    if remainders_found[n] ~= true then
+    if remainders_found[n] then
       idx1 = 0;
       for x = 1,#result do
         idx1 = idx1 + 1
@@ -57,8 +57,6 @@ for i = 2,999 do
         end
       end
       pattern_length = #result - idx1
-      print("HERE " .. tostring(pattern_length) .. " " .. tostring(#result) .. tostring(idx1))
-      print("#result " .. tostring(#result))
       if pattern_length > longest_pattern then
         longest_pattern = pattern_length
         longest_divisor = i
@@ -72,13 +70,6 @@ for i = 2,999 do
     end
   end
 
-  output = "i=" .. tostring(i)
-  resultstr = "0."
-  for x = 1,#result do
-    resultstr = resultstr .. tostring(result[x])
-  end
-  output = output .. " " .. resultstr .. " " .. tostring(pattern_length)
-  print(output)
 end
 
-print(longest_divisor)
+print(longest_divisor)  -- 983
