f5b0935caf9fb4b965d5f5f82aec877fbac07f06
simongarisch <gatman946@gmail.com>
2019-02-25 16:56:51 +1100

    Adding R ex27

--- exercises/Ex027/R.ex27.R
+++ exercises/Ex027/R.ex27.R
@@ -1,13 +1,52 @@
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+is_prime <- function(x){
+  # will return TRUE if x is a prime number, FALSE otherwise
+  if(x <= 1){
+    return(FALSE)
+  }
+  if(x == 2){
+    return(TRUE)
+  }
+  for(i in 3:x-1){
+    if(x %% i == 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
 
-sum <- 0
-for(i in 1:999){
-  if((i %% 3 == 0) || (i %% 5 == 0)){
-    sum <- sum + i
+
+quadratic_formula <- function(n, a, b){
+  # returns the result of n^2 + a*n + b
+  return(n**2 + a*n + b)
+}
+
+
+# where |a| < 1000 and |b| leq 1000
+asearch_range <- -999:999
+bsearch_range <- -1000:1000
+#print(len(asearch_range), len(bsearch_range))  # 1999, 2001
+
+maxprimes <- 0
+product <- 0
+
+for(a in asearch_range){
+  for(b in bsearch_range){
+    n <- 0
+    isprime <- TRUE
+    while(isprime){
+        qfresult <- quadratic_formula(n, a, b)
+        isprime <- is_prime(qfresult)
+
+        if(isprime){
+          n <- n + 1
+          if(n > maxprimes){
+            maxprimes <- n
+            product <- a * b
+          }
+        }
+    }
   }
 }
 
-print(sum)
+print(product)  # -59,231
