8c5cabdea568ab8b9c5e392bb63f56a90b3da429
simongarisch <gatman946@gmail.com>
2019-02-26 18:05:30 +1100

    Adding Octave ex28

--- exercises/Ex028/Octave.ex28.m
+++ exercises/Ex028/Octave.ex28.m
@@ -1,14 +1,28 @@
-%{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-%}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
+function diagsum = get_diagsum(rows)
+  # get the diagonal sum for a given number of layers
+  layers = (rows + 1) / 2;
+  diagsum = 1;
+  layerend_value = 1;
+  layer = 1;
+
+  while layer <= layers
+    numbers_in_layer = ((layer-1) * 2 - 1) * 4 + 4;
+    layerend_value += numbers_in_layer;  # 1, 9, 25, ...
+    diag_steps = (layer * 2) - 2;        # 0, 2, 4, ...
+    top_right = layerend_value;                   # 1, 9, 25, ...
+    top_left = layerend_value - diag_steps;       # 1, 7, 21, ...
+    bot_left = layerend_value - diag_steps * 2;   # 1, 5, 17, ...
+    bot_right = layerend_value - diag_steps * 3;  # 1, 3, 13
+    diagsum += (top_right + top_left + bot_left + bot_right);
+    layer += 1;
   end
+
+  # remove the 1 at the middle of the spiral which we have added 4 times
+  diagsum -= 4
 end
 
-disp(sum) # 233,168
+# in a 5 by 5 spiral there are 3 layers with a diagsum of 101
+#disp(get_diagsum(5))  # 101
+
+disp(get_diagsum(1001))  # 669,171,001
