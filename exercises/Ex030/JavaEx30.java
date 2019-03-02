e341f8795451b41eb3bcd9642783e662b78e0914
simongarisch <gatman946@gmail.com>
2019-03-03 10:11:30 +1100

    Adding JavaEx30

--- exercises/Ex030/JavaEx30.java
+++ exercises/Ex030/JavaEx30.java
@@ -1,19 +1,45 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 */
 
-public class JavaEx1 {
+public class JavaEx30 {
+
+    static int POWER = 5;
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
-          }
+      long ubound = POWER * (long)Math.pow(9, POWER);
+      long total = 0;
+      long num = 2;
+      //System.out.println(digit_powers_equal(1634, 4));  // true
+
+      while(num <= ubound){
+        if(digit_powers_equal(num, POWER)){
+          total += num;
         }
-        System.out.println(sum); // 233,168
+        num += 1;
+      }
+      System.out.println(total);  // 443,839
+    }
+
+    static boolean digit_powers_equal(long num, int power){
+      String numstr = Long.toString(num);
+      long sum_digit_powers = 0;
+      int digit;
+      long increment;
+      for (int i=0; i<numstr.length(); i++){
+        digit = Integer.valueOf(numstr.substring(i, i+1));
+        sum_digit_powers += (long)Math.pow(digit, power);
+      }
+      return num == sum_digit_powers;
     }
 
 }
