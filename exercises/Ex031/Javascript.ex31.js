0df929ba82fdc0a99066ed111e18729cee4060a0
simongarisch <gatman946@gmail.com>
2019-03-04 18:39:00 +1100

    Adding JS ex31

--- exercises/Ex031/Javascript.ex31.js
+++ exercises/Ex031/Javascript.ex31.js
@@ -1,14 +1,39 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+In England the currency is made up of pound and pence, and there are eight coins in general circulation:
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+1p, 2p, 5p, 10p, 20p, 50p, 1 pound (100p) and 2 pound (200p).
+It is possible to make 2 pound in the following way:
+
+1*1 pound + 1*50p + 2*20p + 1*5p + 1*2p + 3*1p
+How many different ways can 2 pound be made using any number of coins?
+
+NOTES:
+Search between 0-1 two pound coins, 0-2 one pound coins... up to 200 1p coins.
+We can look at the cumulative total of value as we add more coins,
+subject to a 2 pound value cap.
+*/
+var TARGET_VALUE = 200
+
+var count_sum_target = 0
+
+for(var v200=0; v200<=TARGET_VALUE; v200+=200){
+  for(var v100=v200; v100<=TARGET_VALUE; v100+=100){
+    for(var v50=v100; v50<=TARGET_VALUE; v50+=50){
+      for(var v20=v50; v20<=TARGET_VALUE; v20+=20){
+        for(var v10=v20; v10<=TARGET_VALUE; v10+=10){
+          for(var v5=v10; v5<=TARGET_VALUE; v5+=5){
+            for(var v2=v5; v2<=TARGET_VALUE; v2+=2){
+              for(var v1=v2; v1<=TARGET_VALUE; v1++){
+                if(v1 == TARGET_VALUE){
+                  count_sum_target++;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
-console.log(sum); // 233,168
+console.log(count_sum_target);  // 73,682
