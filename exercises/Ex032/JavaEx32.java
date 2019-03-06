595d6b3fecb6d12e3d17b0d926ac4efc8cd8cfae
simongarisch <gatman946@gmail.com>
2019-03-06 12:22:34 +1100

    Adding JavaEx32

--- exercises/Ex032/JavaEx32.java
+++ exercises/Ex032/JavaEx32.java
@@ -1,19 +1,61 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once;
+for example, the 5-digit number, 15234, is 1 through 5 pandigital.
 
-public class JavaEx1 {
+The product 7254 is unusual, as the identity, 39 x 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+*/
+import java.util.*;
+
+
+public class JavaEx32{
+
+  static long MAX_MULTIPLICAND = 10000;
+  static long MAX_MULTIPLER = 100;
+
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
+      //System.out.println(is_pandigital(39, 186, 7254));  // true
+      long product;
+      Set<Long> products = new HashSet<Long>();
+      for(long multiplicand=0; multiplicand<=MAX_MULTIPLICAND; multiplicand++){
+        for(long multiplier=0; multiplier<=MAX_MULTIPLER; multiplier++){
+          product = multiplicand * multiplier;
+          if(is_pandigital(multiplicand, multiplier, product)){
+            products.add(product);
           }
         }
-        System.out.println(sum); // 233,168
+      }
+
+      long productsum = 0;
+      Iterator iterator = products.iterator();
+      while (iterator.hasNext()) {
+         productsum += (long)iterator.next();
+      }
+
+      System.out.println(productsum);  // 45,228
+    }
+
+
+    static boolean is_pandigital(long multiplicand, long multiplier, long product){
+      String[] numbers = {
+        Long.toString(multiplicand),
+        Long.toString(multiplier),
+        Long.toString(product)
+      };
+      String numstr = String.join("", numbers);
+      char[] numarr = numstr.toCharArray();
+      Arrays.sort(numarr);
+      numstr = new String(numarr);
+
+      if(numstr.equals("123456789")){
+        return true;
+      }else{
+        return false;
+      }
     }
 
 }
