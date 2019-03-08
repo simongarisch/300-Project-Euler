bc00068c2c6c73fb5861e24ac537565ee5b6b4ac
simongarisch <gatman946@gmail.com>
2019-03-09 07:11:34 +1100

    Adding JS ex33

--- exercises/Ex033/Javascript.ex33.js
+++ exercises/Ex033/Javascript.ex33.js
@@ -1,14 +1,59 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician
+in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction,
+less than one in value, and containing two digits in the numerator and denominator.
+
+If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+var numerators = [];
+var denominators = [];
+for(var denominator=10; denominator<100; denominator++){
+  // less than one in value, so numerator < denominator
+  for(var numerator=10; numerator<denominator; numerator++){
+    // remove non-trivial examples such as 30/50 = 3/5
+    if((numerator % 10 == 0) & (denominator % 10 == 0)){
+      continue;
+    }
+    var result = numerator / denominator;
+    var denstr = denominator.toString();
+    var numstr = numerator.toString();
+    for(const c of numstr){
+      if(denstr.includes(c)){
+        var short_num = numstr.replace(c, "");
+        var short_den = denstr.replace(c, "");
+        if(short_den == "0"){
+          // dodge the zero division error
+          continue;
+        }
+        var new_result = parseInt(short_num) / parseInt(short_den);
+        if(new_result == result){
+          numerators.push(numerator);
+          denominators.push(denominator);
+        }
+      }
+    }
   }
 }
 
-console.log(sum); // 233,168
+// we have the four examples
+//console.log(numerators);    // [ 16, 26, 19, 49 ]
+//console.log(denominators);  // [ 64, 65, 95, 98 ]
+var productnum = numerators.reduce((a, b) => a * b );
+var productden = denominators.reduce((a, b) => a * b );
+var minimum_product = Math.min(productnum, productden);
+for(var divisor=minimum_product; divisor>1; divisor--){
+  if(productnum % divisor == 0){
+    if(productden % divisor == 0){
+      productden /= divisor;
+      productnum /= divisor;
+      break;
+    }
+  }
+}
+
+console.log(productden);  // 100
