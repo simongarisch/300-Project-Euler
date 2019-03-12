5f475b215f3c2281e8168f95f0d9cdc6b701465a
simongarisch <gatman946@gmail.com>
2019-03-13 08:20:41 +1100

    Adding Octave ex34

--- exercises/Ex034/C.ex34.c
+++ exercises/Ex034/C.ex34.c
@@ -1,18 +1,76 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAXCHAR 10
+
+long factorial(int n);
+int is_curious(long x);
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  //printf("%d\n", is_curious(145));  // 1
+  int digits = get_max_digits();
+  long resultcounter = 0;
+  long availableslots = 1;
+  long *curious_numbers = (long *)malloc(availableslots * sizeof(long));
+  for(long x=3; x<pow(10, digits), x++){
+    if(is_curious(x)){
+      resultcounter++;
+      if(resultcounter > availableslots){
+        curious_numbers = (long *)realloc(curious_numbers, resultcounter);
+      }
+      curious_numbers[resultcounter-1] = x;
     }
   }
-  printf("%d\n", sum); // 233,168
+
+  long arrsum = 0; 
+
+  free(curious_numbers);
   return 0;
+}
+
+
+long factorial(int n){
+  if(n == 0){
+    return 1;
+  }else{
+    return n * factorial(n-1);
+  }
+}
+
+
+int is_curious(long x){
+  long sumfact = 0;
+  char xstr[MAXCHAR];
+  sprintf(xstr, "%ld", x);
+  int digit;
+  for(int n=0; n<strlen(xstr); n++){
+    digit = xstr[n] - '0';
+    sumfact = sumfact + factorial(digit);
+  }
+  if(x == sumfact){
+    return TRUE;
+  }else{
+    return FALSE;
+  }
+}
+
+
+int get_max_digits(){
+  int digit = 1;
+  while(factorial(9) * digit > pow(digit - 1, 10)){
+    digit = digit + 1;
+  }
+  return digit;
 }
--- exercises/Ex034/Octave.ex34.m
+++ exercises/Ex034/Octave.ex34.m
@@ -1,14 +1,42 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
+function retval = is_curious(x)
+  # returns True if x is equal to the sum its digit factorials, False otherwise
+  sumfact = 0;
+  xstr = num2str(x);
+  for n = 1:length(xstr)
+    digit = str2num(xstr(n));
+    sumfact += factorial(digit);
   end
-end
+  if x == sumfact
+    retval = true;
+  else
+    retval = false;
+  end
+endfunction
+#is_curious(145)  # true
 
-disp(sum) # 233,168
+
+function digit = get_max_digits(x)
+  digit = 1;
+  while factorial(9) * digit > (digit - 1) ^ 10
+    digit += 1;
+  endwhile
+endfunction
+
+num_digits = get_max_digits();
+curious_numbers = [];
+# as 1! = 1 and 2! = 2 are not sums they are not included
+for x = 3:(10 ^ num_digits)
+  if is_curious(x)
+    curious_numbers = [curious_numbers; x];
+  endif
+endfor
+
+disp(sum(curious_numbers))  # 40,730
