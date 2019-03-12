a3d7e2d2613d46f7301e7c1ae13f1d76ee7b5668
simongarisch <gatman946@gmail.com>
2019-03-12 14:49:24 +1100

    Adding JS ex34

--- exercises/Ex034/Javascript.ex34.js
+++ exercises/Ex034/Javascript.ex34.js
@@ -1,14 +1,46 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+*/
+const math = require("mathjs")
+
+function is_curious(x){
+  // returns True if x is equal to the sum its digit factorials, False otherwise
+  var digits = x.toString().split("");
+  var sumfact = 0;
+  for(var i=0; i<digits.length; i++){
+    var digit = digits[i];
+    sumfact += math.factorial(parseInt(digit));
+  }
+  if(x == sumfact){
+    return true;
+  }else{
+    return false;
+  }
+}
+//console.log(is_curious(145));  // true
+
+
+function get_max_digits(){
+  var digit = 1;
+  while(math.factorial(9) * digit > Math.pow(digit-1, 10)){
+    digit += 1;
+  }
+  return digit;
+}
+
+
+var digits = get_max_digits();
+var curious_numbers = [];
+//  as 1! = 1 and 2! = 2 are not sums they are not included
+for(var x=3; x <= Math.pow(10, digits); x++){
+  if(is_curious(x)){
+    curious_numbers.push(x);
   }
 }
 
-console.log(sum); // 233,168
+var arrsum = curious_numbers.reduce(function(x, y){return x + y});
+console.log(arrsum)  // 40,730
