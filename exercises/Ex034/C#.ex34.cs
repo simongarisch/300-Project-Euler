a376c7322fd2d7b9ca56db2e072a30ea7a254bbe
simongarisch <gatman946@gmail.com>
2019-03-13 08:39:02 +1100

    Adding C# ex34

--- exercises/Ex034/C#.ex34.cs
+++ exercises/Ex034/C#.ex34.cs
@@ -1,19 +1,69 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Ex1{
+
+class Ex34 {
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
+    //Console.WriteLine(is_curious(145));  // true
+    int digits = get_max_digits();
+    List<long> curious_numbers = new List<long>();
+    // as 1! = 1 and 2! = 2 are not sums they are not included
+    for(long x=3; x<Math.Pow(10, digits); x++){
+      if(is_curious(x)){
+        curious_numbers.Add(x);
       }
     }
-    Console.WriteLine(sum); // 233,168
+
+    long numsum = 0;
+    foreach(long number in curious_numbers){
+      numsum += number;
+    }
+    Console.WriteLine(numsum);  // 40,730
   }
+
+
+  static long factorial(int n){
+    if(n == 0){
+      return 1;
+    }else{
+      return n * factorial(n-1);
+    }
+  }
+
+
+  static bool is_curious(long x){
+    // returns True if x is equal to the sum its digit factorials, False otherwise
+    long sumfact = 0;
+    string xstr = x.ToString();
+    int digit;
+    foreach(char c in xstr){
+      digit = c - '0';
+      sumfact += factorial(digit);
+    }
+    if(x == sumfact){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+  static int get_max_digits(){
+    int digit = 1;
+    while(factorial(9) * digit > Math.Pow(digit - 1, 10)){
+      digit = digit + 1;
+    }
+    return digit;
+  }
+
 }
