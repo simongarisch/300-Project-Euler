8ab76de558b2f774552c2d782b90148384122d44
simongarisch <gatman946@gmail.com>
2019-03-15 05:54:10 +1100

    Adding Lua ex35

--- exercises/Ex035/Lua.ex35.lua
+++ exercises/Ex035/Lua.ex35.lua
@@ -1,14 +1,71 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+function is_prime(x)
+  -- will return true if x is prime, false otherwise
+  if x < 2 then
+    return false
+  end
+  if x==2 then
+    return true
+  end
+  for i= 2, math.floor(math.pow(x, 0.5)) do
+    if x % i == 0 then
+      return false
+    end
+  end
+  return true
+end
+
+
+function digit_rotations(x)
+  -- returns a table of results from rotating the digits in x
+  local xstr = tostring(x)
+  local xlen = string.len(xstr)
+  local rotations = {}
+  for i = 1, xlen do
+    local left = string.sub(xstr, 0, i-1)
+    local right = string.sub(xstr, i, xlen)
+    local rotation = tonumber(right .. left)
+    rotations[#rotations + 1] = rotation
+  end
+  return rotations
+end
+--for k,v in pairs(digit_rotations(197)) do print(v) end -- 197, 971, 719
+
+
+function all_primes(numtbl)
+  -- returns true if all of the numbers in numtbl are prime, false otherwise
+  for k,v in pairs(numtbl) do
+    if not is_prime(v) then
+      return false
+    end
+  end
+  return true
+end
+
+
+function is_circular_prime(x)
+  -- returns true if x is a circular prime, false otherwise
+  local rotations = digit_rotations(x)
+  return all_primes(rotations)
+end
+--print(is_circular_prime(197))  -- true
+
+
+counter = 0
+for x = 1, (1e6-1) do
+  if is_prime(x) then
+    if is_circular_prime(x) then
+      counter = counter + 1
+    end
   end
 end
 
-print(sum) -- 233,168
+
+print(counter)  -- 55
