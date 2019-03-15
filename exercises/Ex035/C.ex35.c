95a56df449d422e662ce32f208bffe212146287e
simongarisch <gatman946@gmail.com>
2019-03-16 06:53:04 +1100

    Working on C ex35

--- exercises/Ex035/C.ex35.c
+++ exercises/Ex035/C.ex35.c
@@ -16,28 +16,44 @@ For debugging: https://cets.seas.upenn.edu/answers/gcc.html
 #define TRUE 1
 #define FALSE 0
 #define MAXCHAR 20
+#define TESTNUM 197
 
 int is_prime(long x);
-long *digit_rotations(long x);
+struct Rotations digit_rotations(long x);
+int all_primes(struct Rotations rota);
+int is_circular_prime(long x);
 void slice_str(char *source, char *dest, int start, int end);
 
 
-int main(){
-  //printf("%d\n", is_prime(7));  // 1
-  long *rotations = digit_rotations(197);
-  long entries = sizeof(rotations) / sizeof(rotations[0]);
-  for(long i=0; i<entries; i++){
-    printf("%ld\n", rotations[i]);
-  }
-  free(rotations);
+struct Rotations {
+   long *rotationsarr;
+   long counter;
+};
 
+
+int main(){
+  /*
+  printf("%d\n", is_prime(7));  // 1
+  struct Rotations rota = digit_rotations(TESTNUM);
+  for(long i=0; i<rota.counter; i++){
+    printf("%ld\n", rota.rotationsarr[i]);  // 971, 719, 197
+  }
+  free(rota.rotationsarr);
+  rota.rotationsarr = NULL;
+  */
+
+  long counter = 0;
   for(long x=1; x<1e6; x++){
     if(is_prime(x)){
-      //if(is_circular_prime(x)){
-      //  counter++;
-      //}
+      printf("%d is prime\n", x);
+      if(is_circular_prime(x)){
+        printf("---%d is circular prime\n", x);
+        counter++;
+      }
     }
   }
+
+  printf("%ld\n", counter);
   return 0;
 }
 
@@ -59,34 +75,51 @@ int is_prime(long x){
 }
 
 
-long *digit_rotations(long x){
+struct Rotations digit_rotations(long x){
   // returns an array of results from rotating the digits in x
-  char *xstr;
+  char xstr[MAXCHAR];
   sprintf(xstr, "%ld", x);
   int xlen = strlen(xstr);
-  printf("We are here zZZ!");
 
-  long resultcounter = 0;
-  long availableslots = 1;
-  long *rotations = (long *)malloc(availableslots * sizeof(long));
-  char *left, *right;
-  char rotation[MAXCHAR];
+  long slots = 1;
+  struct Rotations rota;
+  rota.counter = 0;
+  rota.rotationsarr = (long *)malloc(slots * sizeof(long));
+  char left[MAXCHAR], right[MAXCHAR], rotation[MAXCHAR];
   for(int i=1; i<=xlen; i++){
-    printf("We are hereXX!");
     slice_str(xstr, left, 0, i-1);
     slice_str(xstr, right, i, xlen);
     strcpy(rotation, right);
     strcat(rotation, left);
-    printf("left %s\n", left);
-    printf("right %s\n", right);
 
-    resultcounter++;
-    if(resultcounter > availableslots){
-      rotations = (long *)realloc(rotations, resultcounter);
+    rota.counter++;
+    if(rota.counter > slots){
+      rota.rotationsarr = (long *)realloc(rota.rotationsarr, rota.counter);
     }
-    rotations[resultcounter-1] = atol(rotation);
+    rota.rotationsarr[rota.counter-1] = atol(rotation);
   }
-  return rotations;
+  return rota;
+}
+
+
+int all_primes(struct Rotations rota){
+  // returns TRUE if all of the numbers in rota are prime, FALSE otherwise
+  for(long i=0; i<rota.counter; i++){
+    if(!is_prime(rota.rotationsarr[i])){
+      return(FALSE);
+    }
+  }
+  return(TRUE);
+}
+
+
+int is_circular_prime(long x){
+  // returns TRUE if x is a circular prime, FALSE otherwise
+  struct Rotations rota = digit_rotations(x);
+  int is_all_primes = all_primes(rota);
+  free(rota.rotationsarr);
+  rota.rotationsarr = NULL;
+  return(is_all_primes);
 }
 
 
