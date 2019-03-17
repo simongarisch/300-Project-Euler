4cb20250cdb6b545283451bcf23516cf850f0d97
simongarisch <gatman946@gmail.com>
2019-03-18 09:16:57 +1100

    Adding JS ex36

--- exercises/Ex036/Javascript.ex36.js
+++ exercises/Ex036/Javascript.ex36.js
@@ -1,14 +1,38 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+//console.log(Number(585).toString(2));  // 1001001001
+
+function reverse_string(s){
+  var strspl = s.split("");
+  return strspl.reverse().join("");
+}
+//console.log(reverse_string("123"))  // 321
+
+
+function is_both_bases_palindromic(x){
+  var xstr10 = x.toString();
+  var xstr2 = x.toString(2);
+  if(xstr10 == reverse_string(xstr10)){
+    if(xstr2 == reverse_string(xstr2)){
+      return true;
+    }
+  }
+  return false;
+}
+//console.log(is_both_bases_palindromic(585))  // true
+
+
+var totsum = 0;
+for(var x=1; x<1e6; x++){
+  if(is_both_bases_palindromic(x)){
+    totsum += x;
   }
 }
 
-console.log(sum); // 233,168
+console.log(totsum);  // 872,187
