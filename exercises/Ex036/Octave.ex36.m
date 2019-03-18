d27ecb7249f8dde840fee02dd7077c5d12a38ebd
simongarisch <gatman946@gmail.com>
2019-03-19 05:51:13 +1100

    Adding Octave ex36

--- exercises/Ex036/Octave.ex36.m
+++ exercises/Ex036/Octave.ex36.m
@@ -1,14 +1,32 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+#dec2bin(585)  # 1001001001
 
-disp(sum) # 233,168
+function retval = is_both_bases_palindromic(x)
+  # returns true if x is palindromic in base 10 and base 2, false otherwise.
+  xstr10 = num2str(x);
+  xstr2 = dec2bin(x);
+  if strcmp(xstr10, fliplr(xstr10))
+    if strcmp(xstr2, fliplr(xstr2))
+      retval = true;
+      return;
+    endif
+  endif
+  retval = false;
+endfunction
+#is_both_bases_palindromic(585)  # true
+
+totsum = 0;
+for x = 1:1e6
+  if is_both_bases_palindromic(x)
+    totsum += x;
+  endif
+endfor
+
+disp(totsum)  # 872,187
