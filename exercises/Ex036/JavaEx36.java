2344520266e64563eb0d8d019bf5dd41c5977fef
simongarisch <gatman946@gmail.com>
2019-03-19 08:59:47 +1100

    Deleting a couple of files. Working on JavaEx36

--- exercises/Ex036/JavaEx36.java
+++ exercises/Ex036/JavaEx36.java
@@ -1,19 +1,16 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
 
-public class JavaEx1 {
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+*/
+import java.lang.Math; 
+
+public class JavaEx36{
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
-          }
-        }
-        System.out.println(sum); // 233,168
+      System.out.println();
     }
 
 }
