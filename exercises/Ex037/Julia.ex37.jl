9f6c0fd28676c17f281710c2d3f7340363ad7bf4
simongarisch <gatman946@gmail.com>
2019-03-20 09:14:49 +1100

    Adding Julia ex37

--- exercises/Ex037/Julia.ex37.jl
+++ exercises/Ex037/Julia.ex37.jl
@@ -3,13 +3,60 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 =#
+STOP = 11
 
-sum = 0
-for i = 1:999
-    if i % 3 == 0 || i % 5 == 0
-        global sum
-        sum += i
+function isprime(n::Int)::Bool
+    # will return true if x is prime, false otherwise
+    if n < 2
+        return false
     end
+    if n == 2
+        return true
+    end
+    for i::Int in 2:(floor(n ^ 0.5) + 1)
+        if n % i == 0
+            return false
+        end
+    end
+    return true
 end
 
-println(sum) # 233,168
+
+function istruncatable(n::Int)::Bool
+    #= returns true for primes that are both truncatable
+       from left to right and right to left.
+    =#
+    nstr::String = string(n)
+    nlen::Int = length(nstr)
+    for i::Int in 1:nlen
+        left::String = nstr[1:(i-1)]
+        right::String = nstr[i:nlen]
+        #println(left * ", " * right)
+        if length(left) != 0
+            if !isprime(parse(Int,left))
+                return false
+            end
+        end
+        if !isprime(parse(Int, right))
+            return false
+        end
+    end
+    return true
+end
+#println(istruncatable(3797))  # true
+
+
+found = 0
+totsum = 0
+n = 8
+while found < STOP
+    if isprime(n)
+        if istruncatable(n)
+            global found += 1
+            global totsum += n
+        end
+    end
+    global n += 1
+end
+
+println(totsum)  # 748,317
