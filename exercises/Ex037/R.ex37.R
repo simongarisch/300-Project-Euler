039f097abea5630a02dc4a19b011498d991ca4b0
simongarisch <gatman946@gmail.com>
2019-03-20 06:35:55 +1100

    Adding R ex37

--- exercises/Ex037/R.ex37.R
+++ exercises/Ex037/R.ex37.R
@@ -1,13 +1,62 @@
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+#
+# Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+#
+# NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
-sum <- 0
-for(i in 1:999){
-  if((i %% 3 == 0) || (i %% 5 == 0)){
-    sum <- sum + i
+STOP <- 11
+
+isprime <- function(x){
+  if(x < 2){
+    return(FALSE)
   }
+  if(x == 2){
+    return(TRUE)
+  }
+  for(i in 2:as.integer(x ^ 0.5 + 1)){
+    if(x %% i == 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
 }
 
-print(sum) # 233,168
+
+istruncatable <- function(n){
+  # returns true for primes that are both truncatable
+  # from left to right and right to left.
+  nstr <- as.character(n)
+  nlen <- nchar(nstr)
+  for(i in 1:nlen){
+    left <- substr(nstr, 1, i-1)
+    right <- substr(nstr, i, nlen)
+    #print(paste(left, right))
+    if(nchar(left) != 0){
+      if(!isprime(as.numeric(left))){
+        return(FALSE)
+      }
+    }
+    if(!isprime(as.numeric(right))){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+#print(istruncatable(3797))  # TRUE
+
+
+found <- 0
+totsum <- 0
+n <- 8
+while(found < STOP){
+  if(isprime(n)){
+    if(istruncatable(n)){
+      found <- found + 1
+      totsum <- totsum + n
+    }
+  }
+  n = n + 1
+}
+
+print(totsum)  # 748,317
