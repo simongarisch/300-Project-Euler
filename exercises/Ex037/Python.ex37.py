8a53ee883cc2865f36e6ed96e2052c84d1c5a813
simongarisch <gatman946@gmail.com>
2019-03-20 06:20:27 +1100

    Adding Python and Readme ex37

--- exercises/Ex037/Python.ex37.py
+++ exercises/Ex037/Python.ex37.py
@@ -1,12 +1,40 @@
 '''
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 '''
+from sympy import isprime
 
-sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
+STOP = 11
 
-print(sum) # 233,168
+def istruncatable(n):
+    ''' returns true for primes that are both truncatable
+        from left to right and right to left.
+    '''
+    nstr = str(n)
+    for i in range(len(nstr)):
+        left = nstr[:i]
+        right = nstr[i:]
+        #print(left, right)
+        if left != "":
+            if not isprime(int(left)):
+                return False
+        if not isprime(int(right)):
+            return False
+    return True
+
+#print(istruncatable(3797))  # True
+
+found = 0
+totsum = 0
+n = 8
+while found < STOP:
+    if isprime(n):
+        if istruncatable(n):
+            found += 1
+            totsum += n
+    n += 1
+
+print(totsum)  # 748,317
