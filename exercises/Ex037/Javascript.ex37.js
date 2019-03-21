08019aba9f153ea97de52e47cf76dd4d45307d33
simongarisch <gatman946@gmail.com>
2019-03-22 05:58:10 +1100

    Adding JS ex37

--- exercises/Ex037/Javascript.ex37.js
+++ exercises/Ex037/Javascript.ex37.js
@@ -1,14 +1,67 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+*/
+var STOP = 11;
+
+function isprime(n){
+  // will return true if number is prime, false otherwise
+  if(n<2){
+    return false;
   }
+  if(n==2){
+    return true;
+  }
+
+  for(var i=2; i<=Math.pow(n, 0.5); i++){
+    if(n % i == 0){
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(sum); // 233,168
+
+function istruncatable(n){
+  // returns true for primes that are both truncatable
+  // from left to right and right to left.
+  var nstr = n.toString();
+  var nlen = nstr.length;
+  for(var i=0; i<nlen; i++){
+    var left = nstr.substring(0,i);
+    var right = nstr.substring(i, nlen);
+    //console.log(left + ", " + right)
+
+    if(left.length > 0){
+      if(!isprime(parseInt(left))){
+        return false;
+      }
+    }
+    if(right.length > 0){
+      if(!isprime(parseInt(right))){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+//console.log(istruncatable(3797));  // true
+
+
+var found = 0;
+var totsum = 0;
+var n = 8;
+while(found < STOP){
+  if(isprime(n)){
+    if(istruncatable(n)){
+      found++;
+      totsum += n;
+    }
+  }
+  n++;
+}
+
+console.log(totsum)  // 748,317
