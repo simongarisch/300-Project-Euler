887def4d41292c89821a989734916f9fb30efc75
simongarisch <gatman946@gmail.com>
2019-03-24 07:01:53 +1100

    Working on Julia ex38

--- exercises/Ex038/Julia.ex38.jl
+++ exercises/Ex038/Julia.ex38.jl
@@ -1,15 +1,21 @@
 #=
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 * 1 = 192
+192 * 2 = 384
+192 * 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576.
+We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5,
+giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the
+concatenated product of an integer with (1,2, ... , n) where n > 1?
 =#
 
-sum = 0
-for i = 1:999
-    if i % 3 == 0 || i % 5 == 0
-        global sum
-        sum += i
-    end
+function pandigital(x, n)
+    # returns pandigital value if ispandigital
+    # 0 otherwise
+    
 end
-
-println(sum) # 233,168
