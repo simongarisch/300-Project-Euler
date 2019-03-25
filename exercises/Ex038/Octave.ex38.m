3208095ed585a10698cb894905a300a0c5026767
simongarisch <gatman946@gmail.com>
2019-03-26 06:52:46 +1100

    Adding Octave ex38

--- exercises/Ex038/Octave.ex38.m
+++ exercises/Ex038/Octave.ex38.m
@@ -1,14 +1,54 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 * 1 = 192
+192 * 2 = 384
+192 * 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576.
+We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5,
+giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the
+concatenated product of an integer with (1,2, ... , n) where n > 1?
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+function pnum = pandigital(x, n)
+  # returns pandigital value if ispandigital
+  # 0 otherwise
+  result = "";
+  for i = 1:n
+    result = strcat(result, num2str(x * i));
+  endfor
+  pnum = str2num(result);
+  concat = sort(result);
+  if strcmp(concat, "123456789")
+    return;
+  else  
+    pnum = 0;
+  endif
+endfunction
+#pandigital(192, 3)  # 192384576
 
-disp(sum) # 233,168
+
+function pnum = maxpandigitaln(x)
+  # check if x is pandigital for any value of n
+  # where n > 1
+  # because we are doing a concat, n < 10
+  pnum = 0;
+  for n = 2:10
+    pnum = max(pnum, pandigital(x,n));
+  endfor
+endfunction
+#maxpandigitaln(192)  # 192384576
+
+
+x = 98765;
+maxpan = 0;
+while x > 0
+  maxpan = max(maxpan, maxpandigitaln(x));
+  x -= 1;
+endwhile
+
+disp(maxpan)
