eb43cf2f0b0024e4cbe75869da4c9be4ad757dac
simongarisch <gatman946@gmail.com>
2019-03-26 09:19:16 +1100

    Working in C ex38

--- exercises/Ex038/C.ex38.c
+++ exercises/Ex038/C.ex38.c
@@ -1,18 +1,49 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 * 1 = 192
+192 * 2 = 384
+192 * 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576.
+We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5,
+giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the
+concatenated product of an integer with (1,2, ... , n) where n > 1?
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAXCHAR 20
+
+unsigned long long int pandigital(int x, int n);
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
-  }
-  printf("%d\n", sum); // 233,168
+  pandigital(192, 3);  // 192384576
   return 0;
+}
+
+
+unsigned long long int pandigital(int x, int n){
+  // returns pandigital value if ispandigital
+  // 0 otherwise
+  char results[MAXCHAR];
+  results[0] = '\0';
+  char strmult[MAXCHAR];
+  int mult;
+  for(int i=1; i<=n; i++){
+    mult = x * i;
+    sprintf(strmult, "%d", mult);
+    strcat(results, strmult);
+  }
+  //printf("%s\n", results);
+  char **pend;
+  unsigned long long int pnum = strtoull(results, pend, 10);
+  return pnum;
 }
