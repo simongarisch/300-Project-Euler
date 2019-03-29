58487d33bbcd40f890fa46e655247dddf4eaeb25
simongarisch <gatman946@gmail.com>
2019-03-30 11:04:58 +1300

    Adding JS ex39

--- exercises/Ex039/Javascript.ex39.js
+++ exercises/Ex039/Javascript.ex39.js
@@ -1,14 +1,35 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c},
+there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p <= 1000, is the number of solutions maximised?
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+var perimeters = {};
+for(var a=1; a<1000; a++){
+  for(var b=a+1; b<(1000-a); b++){
+    for(var c=b+1; c<(1000-a-b); c++){
+      if(Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)){
+        var perimeter = (a + b + c).toString();
+        if(perimeter in perimeters){
+          perimeters[perimeter] += 1;
+        }else{
+          perimeters[perimeter] = 1;
+        }
+      }
+    }
   }
 }
 
-console.log(sum); // 233,168
+var maxp = 0;
+var maxn = 0;
+for (const [k, v] of Object.entries(perimeters)) {
+  if(v > maxn){
+    maxn = v;
+    maxp = parseInt(k);
+  }
+}
+
+console.log(maxp);  // 840
