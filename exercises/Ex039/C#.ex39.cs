a8bacdb621cddafdbde8cbc94ec36a590aad4f14
simongarisch <gatman946@gmail.com>
2019-03-31 19:46:26 +1300

    Adding C# ex39

--- exercises/Ex039/C#.ex39.cs
+++ exercises/Ex039/C#.ex39.cs
@@ -1,19 +1,41 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c},
+there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p <= 1000, is the number of solutions maximised?
 */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Ex1{
+class Ex39{
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
+    var perimeters = Enumerable.Repeat<long>(1, 1001).ToArray();
+    long perimeter;
+    for(int a=1; a<=1000; a++){
+      for(int b=(a+1); b<=(1000-a); b++){
+        for(int c=(b+1); c<=(1000-a-b); c++){
+          if(Math.Pow(a,2) + Math.Pow(b,2) == Math.Pow(c,2)){
+            perimeter = a + b + c;
+            perimeters[perimeter] = perimeters[perimeter] + 1;
+          }
+        }
       }
     }
-    Console.WriteLine(sum); // 233,168
+
+    long v, maxp=0, maxn=0;
+    for(int i=1; i<=1000; i++){
+      v = perimeters[i];
+      if(v > maxn){
+        maxn = v;
+        maxp = i;
+      }
+    }
+    Console.WriteLine(maxp);  // 840
   }
+
 }
