a55930cf8e9793fc0744a11629b56329de25d621
simongarisch <gatman946@gmail.com>
2019-04-05 06:07:23 +1100

    Adding JS ex40

--- exercises/Ex040/Javascript.ex40.js
+++ exercises/Ex040/Javascript.ex40.js
@@ -1,14 +1,36 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.12345678910 1 112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+var num = 0;
+var digit_counter = 0;
+var mult = 1;
+var result = 1;
+
+while(mult <= 1e6){
+  var found = false;
+  while(!found){
+    num++;
+    var numstr = num.toString();
+    var numspl = numstr.split("");
+    for(var i=0; i<numspl.length; i++){
+      var digit = parseInt(numspl[i]);
+      digit_counter++;
+      if(mult == digit_counter){
+        result *= digit;
+        found = true;
+        //console.log(digit_counter, digit);
+      }
+    }
   }
+  mult *= 10;
 }
 
-console.log(sum); // 233,168
+console.log(result);  // 210
