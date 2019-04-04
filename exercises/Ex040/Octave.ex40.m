8395951b25b29b502fa31f8c9ad952a6dbe2ff7e
simongarisch <gatman946@gmail.com>
2019-04-05 06:19:53 +1100

    Adding Octave ex40

--- exercises/Ex040/Octave.ex40.m
+++ exercises/Ex040/Octave.ex40.m
@@ -1,14 +1,34 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.12345678910 1 112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+num = 0;
+digit_counter = 0;
+mult = 1;
+result = 1;
 
-disp(sum) # 233,168
+while mult <= 1e6
+  found = false;
+  while !found
+    num += 1;
+    numstr = num2str(num);
+    for i = 1:length(numstr)
+      digit = str2num(numstr(i));
+      digit_counter += 1;
+      if mult == digit_counter
+        result *= digit;
+        found = true;
+      endif
+    endfor
+  endwhile
+  mult *= 10;
+endwhile
+
+disp(result)  # 210
