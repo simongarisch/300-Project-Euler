e169e0a0ed4228521fa711a14ff7b518110efd20
simongarisch <gatman946@gmail.com>
2019-04-08 07:05:21 +1000

    Working on R ex41

--- exercises/Ex041/R.ex41.R
+++ exercises/Ex041/R.ex41.R
@@ -5,9 +5,37 @@
 # What is the largest n-digit pandigital prime that exists?
 library(matlab)
 library(gtools)
+library(stringr)
+library(stringi)
 
 DIGITS <- "123456789"
 
-print(matlab::isprime(7))
-v = base::strsplit(DIGITS, "")[[1]]
-print(gtools::permutations(n=3,r=3,v=v,repeats.allowed=F))
+#print(matlab::isprime(7))
+#v = base::strsplit(DIGITS, "")[[1]]
+#print(gtools::permutations(n=3,r=3,v=v,repeats.allowed=F))
+
+n <- nchar(DIGITS)
+largest <- 0
+
+while(n>=1 & largest==0){
+  digits <- stringr::str_sub(DIGITS,start=1,end=n)
+  digits <- base::strsplit(digits, "")[[1]]
+  perms <- gtools::permutations(n=n, r=n, v=digits, repeats.allowed=F)
+  print(digits)
+  print(length(perms))
+  print(n)
+  print("***")
+  for(perm in perms){
+    #print(perm)
+    val <- stringi::stri_paste(perm, collapse="")
+    #print(val)
+    val <- as.numeric(val)
+    if(matlab::isprime(val)){
+      largest <- val
+      break
+    }
+  }
+  n <- n - 1
+}
+
+print(largest)  #
