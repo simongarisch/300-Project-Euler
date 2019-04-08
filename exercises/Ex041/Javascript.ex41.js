863be49062a6c68b9bf5404c08fe578886c40bd3
simongarisch <gatman946@gmail.com>
2019-04-09 05:56:15 +1000

    Adding js ex41

--- exercises/Ex041/Javascript.ex41.js
+++ exercises/Ex041/Javascript.ex41.js
@@ -1,14 +1,47 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+We shall say that an n-digit number is pandigital if it makes use of all the
+digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+What is the largest n-digit pandigital prime that exists?
+*/
+var Combinatorics = require("js-combinatorics");
+
+var DIGITS = "123456789";
+
+
+function isprime(n){
+  // will return true if n is prime, false otherwise
+  if(n<2){
+    return false;
   }
+  if(n==2){
+    return true;
+  }
+
+  for(var i=2; i<=Math.pow(n, 0.5); i++){
+    if(n % i == 0){
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(sum); // 233,168
+
+var n = DIGITS.length;
+var largest = 0;
+
+while(n >= 1 && largest==0){
+  var digits = DIGITS.slice(0, n-1);
+  var perms = Combinatorics.permutation(digits.split(""));
+  perms = perms.toArray().reverse();
+  for(var i=0; i<perms.length; i++){
+    var val = parseInt(perms[i].join(""));
+    if(isprime(val)){
+      largest = val;
+      break;
+    }
+  }
+  n--;
+}
+
+console.log(largest);  // 7,652,413
