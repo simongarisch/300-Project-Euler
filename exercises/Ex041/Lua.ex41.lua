8dbb8af0eedb67ccfd3fecc436129f51cdd96fe2
simongarisch <gatman946@gmail.com>
2019-04-09 06:47:20 +1000

    Adding Lua ex41

--- exercises/Ex041/Lua.ex41.lua
+++ exercises/Ex041/Lua.ex41.lua
@@ -1,14 +1,81 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+We shall say that an n-digit number is pandigital if it makes use of all the
+digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+What is the largest n-digit pandigital prime that exists?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+DIGITS = "123456789"
+
+
+function isprime(n)
+  -- will return true if n is prime, false otherwise
+  if n < 2 then
+    return false
+  end
+  if n==2 then
+    return true
+  end
+  for i= 2, math.floor(math.pow(n, 0.5)) do
+    if n % i == 0 then
+      return false
+    end
+  end
+  return true
+end
+
+
+function collect_characters(tbl)
+  -- concatenate and return the characters from a given table
+  -- for example, {'1', '2', '3'} -> '123'
+  local numstr = ""
+  for k,v in pairs(tbl) do
+    numstr = numstr .. tostring(v)
+  end
+  return numstr
+end
+
+
+function permgen(a, n)
+  -- collect permutations from a given table a
+  -- this is borrowed from https://www.lua.org/pil/9.3.html
+  if n == 0 then
+    perms[#perms+1] = tonumber(collect_characters(a))
+  end
+  for i=1,n do
+    -- put i-th element as the last one
+    a[n], a[i] = a[i], a[n]
+
+    -- generate all permutations of the other elements
+    permgen(a, n - 1)
+
+    -- restore i-th element
+    a[n], a[i] = a[i], a[n]
   end
 end
 
-print(sum) -- 233,168
+
+n = string.len(DIGITS)
+largest = 0
+
+while n>=1 and largest==0 do
+  digits = string.sub(DIGITS, 1, n)
+  numbers = {}
+  for i=1, #digits do
+    numbers[i] = string.sub(digits, i, i)
+  end
+  perms = {}
+  permgen(numbers, n)
+  table.sort(perms, function(a, b) return a > b end)
+  for k,v in pairs(perms) do
+    --print(v)
+    val = tonumber(v)
+    if isprime(val) then
+      largest = val
+      break
+    end
+  end
+  n = n - 1
+end
+
+print(largest)  -- 7,652,413
