4f3b902fe121aa47ddadc8b6dc4d1170b66106ce
simongarisch <gatman946@gmail.com>
2019-04-08 10:49:34 +1000

    Adding R ex41

--- exercises/Ex041/R.ex41.R
+++ exercises/Ex041/R.ex41.R
@@ -3,7 +3,6 @@
 # digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
 #
 # What is the largest n-digit pandigital prime that exists?
-library(matlab)
 library(gtools)
 library(stringr)
 library(stringi)
@@ -17,20 +16,32 @@ DIGITS <- "123456789"
 n <- nchar(DIGITS)
 largest <- 0
 
+
+isprime <- function(x){
+  if(x < 2){
+    return(FALSE)
+  }
+  if(x == 2){
+    return(TRUE)
+  }
+  for(i in 2:as.integer(x ^ 0.5 + 1)){
+    if(x %% i == 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+
+
 while(n>=1 & largest==0){
   digits <- stringr::str_sub(DIGITS,start=1,end=n)
   digits <- base::strsplit(digits, "")[[1]]
   perms <- gtools::permutations(n=n, r=n, v=digits, repeats.allowed=F)
-  print(digits)
-  print(length(perms))
-  print(n)
-  print("***")
-  for(perm in perms){
-    #print(perm)
+  for(row in nrow(perms):1){
+    perm <- perms[row,]
     val <- stringi::stri_paste(perm, collapse="")
-    #print(val)
     val <- as.numeric(val)
-    if(matlab::isprime(val)){
+    if(isprime(val)){
       largest <- val
       break
     }
@@ -38,4 +49,4 @@ while(n>=1 & largest==0){
   n <- n - 1
 }
 
-print(largest)  #
+print(largest)  # 7,652,413
