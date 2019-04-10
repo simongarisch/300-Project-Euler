b3a4f873796345f6ecc342bf06594640a9f142ff
simongarisch <gatman946@gmail.com>
2019-04-11 06:30:58 +1000

    Adding C# ex41

--- exercises/Ex041/C#.ex41.cs
+++ exercises/Ex041/C#.ex41.cs
@@ -1,19 +1,82 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+We shall say that an n-digit number is pandigital if it makes use of all the
+digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+What is the largest n-digit pandigital prime that exists?
 */
-
 using System;
+using System.Collections.Generic;
 
-class Ex1{
+
+class Ex41{
+
+  static string DIGITS = "123456789";
+  static List<String> perms = new List<String>();
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
+    int n = DIGITS.Length;
+    long largest = 0;
+    string numstr;
+    long perm;
+
+    while(n>=1 && largest==0){
+      perms.Clear();
+      numstr = DIGITS.Substring(0, n);
+      permgen(numstr, numstr.Length);
+      perms.Sort();
+      for(int i=0; i<perms.Count; i++){
+        //Console.WriteLine(perms[i]);
+        perm = long.Parse(perms[i]);
+        if(isprime(perm)){
+          largest = perm;
+        }
+      }
+      n--;
+    }
+
+    Console.WriteLine(largest);  // 7,652,413
+  }
+
+
+  private static void permgen(String numstr, int n){
+    if (n == 0){
+      perms.Add(numstr);
+      //Console.WriteLine(numstr);
+    }else{
+      for(int i=0; i<n; i++){
+        numstr = swap(numstr, i, n-1);
+        permgen(numstr, n-1);
+        numstr = swap(numstr, i, n-1);
       }
     }
-    Console.WriteLine(sum); // 233,168
   }
+
+
+  private static String swap(String numstr, int i, int j){
+    char temp;
+    char[] chars = numstr.ToCharArray();
+    temp = chars[i];
+    chars[i] = chars[j];
+    chars[j] = temp;
+    string s = new string(chars);
+    return s;
+  }
+
+
+  static bool isprime(long n){
+    // will return true if n is prime, false otherwise
+    if(n < 2){
+      return false;
+    }
+    if(n == 2){
+      return true;
+    }
+    for(long i=2; i<=Math.Pow(n,0.5); i++){
+      if(n % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
