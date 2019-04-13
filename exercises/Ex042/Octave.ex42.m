12e9f5b14672da10a41359253bb77e538204c77e
simongarisch <gatman946@gmail.com>
2019-04-13 12:08:20 +1000

    Working on Cotave ex42

--- exercises/Ex042/Octave.ex42.m
+++ exercises/Ex042/Octave.ex42.m
@@ -1,14 +1,78 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The nth term of the sequence of triangle numbers is given by,
+tn = 0.5n(n+1); so the first ten triangle numbers are:
+
+1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+
+By converting each letter in a word to a number corresponding to its
+alphabetical position and adding these values we form a word value.
+For example, the word value for SKY is 19 + 11 + 25 = 55 = t10.
+If the word value is a triangle number then we shall call the word a triangle word.
+
+Using words.txt (right click and 'Save Link/Target As...'),
+a 16K text file containing nearly two-thousand common English words,
+how many are triangle words?
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+dire_path = fileparts(mfilename("fullpath"));
+file_path = strcat(dire_path, "/words.txt")
+#exist(file_path)
 
-disp(sum) # 233,168
+content = "";
+fi = fopen(file_path, "r");
+while true
+  # Get the current line
+  ln = fgetl(fi);
+  content = strcat(content, ln);
+  
+  # Stop if EOFS
+  if ln == -1
+    break;
+  endif
+endwhile
+content = strrep(content, '"', "");
+content = strrep(content, ' ', "");
+words = sort(strsplit(content, ","));
+
+tnums = cumsum(cumsum(ones(1,100)));
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+scores = 1:length(alphabet);
+global scores_mapping = struct();
+for i = scores
+  letter = alphabet(i);
+  scores_mapping.(letter) = i;
+endfor
+
+
+function word_score = get_word_score(word)
+  global scores_mapping;
+  global alphabet;
+  word_score = 0;
+  for i =1:length(word)
+    char = substr(word, i, 1);
+    try
+      word_score += scores_mapping.(char);
+    catch
+      #printf ("Unable to find char: %s\n", char)
+    end
+  endfor
+endfunction
+
+
+function triangle_word = is_triangle_word(word)
+  global tnums;
+  score = get_word_score(word);
+  triangle_word = sum(tnums == score);
+endfunction
+
+
+total_words = 0;
+for i = 1:length(words)
+  word = words{1,i};
+  if is_triangle_word(word)
+    total_words += 1;
+  endif
+endfor
+
+disp(total_words)
