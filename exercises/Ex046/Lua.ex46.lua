6032c0f1cdd7a8cb5f7b4e605755c431c0896c03
simongarisch <gatman946@gmail.com>
2019-05-03 06:09:23 +1000

    Adding Lua ex46

--- exercises/Ex046/Lua.ex46.lua
+++ exercises/Ex046/Lua.ex46.lua
@@ -1,14 +1,101 @@
 --[[
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+A composite number is a positive integer that can be formed by multiplying
+two smaller positive integers.
+It was proposed by Christian Goldbach that every odd composite number
+can be written as the sum of a prime and twice a square.
+
+9 = 7 + 2 * 1^2
+15 = 7 + 2 * 2^2
+21 = 3 + 2 * 3^2
+25 = 7 + 2 * 3^2
+27 = 19 + 2 * 2^2
+33 = 31 + 2 * 1^2
+
+It turns out that the conjecture was false.
+
+What is the smallest odd composite that cannot be written as
+the sum of a prime and twice a square?
 --]]
 
-sum = 0
-for i = 1, 999 do
-  if(i % 3 == 0 or i % 5 == 0) then
-    sum = sum + i
+MAXN = 1e4
+
+function isprime(n)
+  -- will return true if n is prime, false otherwise
+  if n < 2 then
+    return false
   end
+  if n==2 then
+    return true
+  end
+  for i= 2, math.floor(math.pow(n, 0.5)) do
+    if n % i == 0 then
+      return false
+    end
+  end
+  return true
 end
 
-print(sum) -- 233,168
+
+function generate_odd_compos(n)
+  local compos = {}
+  for i=2,n do
+    for j=2,n do
+      local mult = i * j
+      if mult % 2 == 1 then
+        compos[mult] = mult
+      end
+    end
+  end
+  return compos
+end
+
+
+function generate_primes(n)
+  local primes = {}
+  for i=1,n do
+    if isprime(i) then
+      primes[#primes+1] = i
+    end
+  end
+  return primes
+end
+
+
+function generate_squares(n)
+  local squares = {}
+  for i=1,math.floor(n^0.5) do
+    squares[#squares+1] = i^2
+  end
+  return squares
+end
+
+
+function find_smallest(n)
+  -- find the smallest odd composite that cannot be written as
+  -- the sum of a prime and twice a square
+  local compos = generate_odd_compos(n)
+  local primes = generate_primes(n)
+  local squares = generate_squares(n)
+  local search_tbl = {}
+
+  for i=1,#primes do
+    for j=1,#squares do
+      local comb = primes[i] + 2*squares[j]
+      search_tbl[comb] = comb
+    end
+  end
+
+  local smallest = 0
+  for k,compo in pairs(compos) do
+    if search_tbl[compo] == nil then
+      smallest = compo
+      break
+    end
+  end
+
+  return smallest
+end
+
+
+smallest = find_smallest(MAXN)
+print(smallest)  -- 5,777
