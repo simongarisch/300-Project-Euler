4184e7ebdd3319ab49dc036354f330fefad175a1
simongarisch <gatman946@gmail.com>
2019-05-10 09:09:40 +1000

    Adding R ex47

--- exercises/Ex047/R.ex47.R
+++ exercises/Ex047/R.ex47.R
@@ -12,3 +12,68 @@
 #
 # Find the first four consecutive integers to have four distinct prime factors each.
 # What is the first of these numbers?
+library(matlab)
+
+TARGET_FACTORS = 4
+MAXPRIME = 1e3
+
+
+primes <- c()
+for(i in 1:MAXPRIME){
+  if(matlab::isprime(i)){
+    primes <- c(primes, i)
+  }
+}
+
+
+mult_factors <- function(prime_factors){
+  # return the results we get from multiplying prime factors
+  result <- 1
+  for(prime in prime_factors){
+    # straight multiplication
+    result <- result * prime
+  }
+
+  results <- c(result)
+  for(prime in prime_factors){
+    # where we have ^2
+    results <- c(results, result * prime)
+  }
+  return(results)
+}
+
+
+sequence <- 0
+n <- 0
+while(TRUE){
+  n <- n + 1
+  factors <- 0
+  prime_factors <- c()
+  for(prime in primes){
+    if(n %% prime == 0){
+      factors <- factors + 1
+      prime_factors <- c(prime_factors, prime)
+    }
+    if(prime > n){
+      break
+    }
+  }
+
+  if(factors == TARGET_FACTORS){
+    multiples <- mult_factors(prime_factors)
+    if(n %in% multiples){
+      sequence <- sequence + 1
+    }else{
+      sequence <- 0
+    }
+  }else{
+    sequence <- 0
+  }
+
+  if(sequence == TARGET_FACTORS){
+    break
+  }
+}
+
+first <- n - TARGET_FACTORS + 1
+print(first)  # 134,043
