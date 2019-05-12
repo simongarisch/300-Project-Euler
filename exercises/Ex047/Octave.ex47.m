3486b1b6673a4498d57b800ae84289439fea06e6
simongarisch <gatman946@gmail.com>
2019-05-12 13:56:43 +1000

    Adding Octave ex47

--- exercises/Ex047/Octave.ex47.m
+++ exercises/Ex047/Octave.ex47.m
@@ -1,14 +1,47 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct prime factors each.
+What is the first of these numbers?
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+TARGET_FACTORS = 4;
+MAXPRIME = 1e3;
 
-disp(sum) # 233,168
+primes = (1:MAXPRIME)(find(isprime(1:MAXPRIME)));
+
+#prod(x) * [1, x]
+
+sequence = 0;
+n = 0;
+
+while true
+   n = n + 1;
+   prime_factors = primes(find(mod(n, primes) == 0));
+   if length(prime_factors) == TARGET_FACTORS
+     multiples = prod(prime_factors) * [1, prime_factors];
+     if any(multiples == n)
+       sequence += 1;
+     else
+       sequence = 0;
+     endif
+   else
+     sequence = 0;
+   endif
+   
+   if sequence == TARGET_FACTORS
+     break;
+   endif   
+endwhile
+
+first = n - TARGET_FACTORS + 1;
+disp(first)  # 134,043
