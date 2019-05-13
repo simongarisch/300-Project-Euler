c8b07201d24cd1b25e0971af04375f0354c48490
simongarisch <gatman946@gmail.com>
2019-05-14 06:54:09 +1000

    Adding C++ ex47

--- exercises/Ex047/C++.ex47.cpp
+++ exercises/Ex047/C++.ex47.cpp
@@ -1,18 +1,115 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+The first two consecutive numbers to have two distinct prime factors are:
 
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct prime factors each.
+What is the first of these numbers?
+*/
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+
+#define TARGET_FACTORS 4
+#define MAXPRIME 1e3
+
+using namespace std;
+
+bool isprime(long x);
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  // collect our prime numbers
+  long numprimes = 0;
+  vector<long> primes = {};
+  for(long i=0; i<MAXPRIME; i++){
+    if(isprime(i)){
+      primes.push_back(i);
     }
   }
-  std::cout << sum << std::endl; // 233,168
+  //for(long prime : primes){
+  //  cout << prime << endl;
+  //}
+
+  long sequence = 0;
+  long n = 0;
+  vector<long> multiples_vector;
+  vector<long> factors_vector;
+  bool multiple_found;  // is n within multiples
+
+  while(true){
+    n++;
+    // reset the factors vector
+    factors_vector = {};
+
+    for(long prime : primes){
+      if(n % prime == 0){
+        factors_vector.push_back(prime);
+      }
+      if(prime > n){
+        break;
+      }
+    }
+
+    if(factors_vector.size() == TARGET_FACTORS){
+      multiples_vector = {};
+      multiples_vector.push_back(1);
+      // straight multiplication
+      for(long factor : factors_vector){
+        multiples_vector.at(0) = multiples_vector.at(0) * factor;
+      }
+      // and for the ^2
+      for(long factor : factors_vector){
+        multiples_vector.push_back(multiples_vector.at(0) * factor);
+      }
+
+      multiple_found = false;
+      for(long multiple : multiples_vector){
+        if(n == multiple){
+          multiple_found = true;
+        }
+      }
+
+      if(multiple_found){
+        sequence++;
+      }else{
+        sequence = 0;
+      }
+    }else{
+      sequence = 0;
+    }
+
+    if(sequence == TARGET_FACTORS){
+      break;
+    }
+  }
+
+  long first = n - TARGET_FACTORS + 1;
+  cout << first << endl;  // 134,043
   return 0;
+}
+
+
+bool isprime(long x){
+  // will return true if x is prime, false otherwise
+  if(x < 2){
+    return false;
+  }
+  if(x == 2){
+    return true;
+  }
+  for(long i=2; i<=pow(x, 0.5); i++){
+    if(x % i == 0){
+      return false;
+    }
+  }
+  return true;
 }
