cda927f1ea3229ab8953948e2e2bd7debe9c7945
simongarisch <gatman946@gmail.com>
2019-05-15 05:47:40 +1000

    Adding Java ex47

--- exercises/Ex047/JavaEx47.java
+++ exercises/Ex047/JavaEx47.java
@@ -1,19 +1,113 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+The first two consecutive numbers to have two distinct prime factors are:
 
-public class JavaEx1 {
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct prime factors each.
+What is the first of these numbers?
+*/
+import java.util.*;
+
+
+public class JavaEx47{
+
+
+    static int TARGET_FACTORS = 4;
+    static int MAXPRIME = (int)1e3;
+
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
+      List<Long> primes = new ArrayList<Long>();
+      for(long i=1; i<=MAXPRIME; i++){
+        if(isprime(i)){
+          primes.add(i);
+          //System.out.println(i);
+        }
+      }
+
+      long sequence = 0;
+      long n = 0;
+      long prime;
+      List<Long> factorsList = new ArrayList<Long>();
+      List<Long> multiples = new ArrayList<Long>();
+      while(true){
+        n += 1;
+        factorsList.clear();
+        multiples.clear();
+
+        for(int i=0; i<primes.size(); i++){
+          prime = primes.get(i);
+          if(n % prime == 0){
+            factorsList.add(prime);
+          }
+          if(prime > n){
+            break;
           }
         }
-        System.out.println(sum); // 233,168
+
+        if(factorsList.size() == TARGET_FACTORS){
+          multiples = multFactors(factorsList);
+          if(multiples.contains(n)){
+            sequence += 1;
+          }else{
+            sequence = 0;
+          }
+        }else{
+          sequence = 0;
+        }
+
+        if(sequence == TARGET_FACTORS){
+          break;
+        }
+      }
+
+      long first = n - TARGET_FACTORS + 1;
+      System.out.println(first);  // 134,043
+    }
+
+
+    public static List<Long> multFactors(List<Long> factorsList){
+      // return the results we get from multiplying prime factors
+      List<Long> results = new ArrayList<Long>();
+      long prime, result = 1;
+
+      // straight multiplication
+      for(int i=0; i<factorsList.size(); i++){
+        prime = factorsList.get(i);
+        result *= prime;
+      }
+      results.add(result);
+
+      // where we have a ^2
+      for(int i=0; i<factorsList.size(); i++){
+        prime = factorsList.get(i);
+        results.add(result * prime);
+      }
+      return results;
+    }
+
+
+    public static boolean isprime(long x){
+      // will return true if x is prime, false otherwise
+      if(x < 2){
+        return false;
+      }
+      if(x == 2){
+        return true;
+      }
+      for(long i=2; i<=Math.pow(x, 0.5); i++){
+        if(x % i == 0){
+          return false;
+        }
+      }
+      return true;
     }
 
 }
