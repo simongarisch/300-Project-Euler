07e7f15e361434bd0594f3244ba4554685aa64fb
simongarisch <gatman946@gmail.com>
2019-05-19 07:21:44 +1000

    Adding C# ex48

--- exercises/Ex048/C#.ex48.cs
+++ exercises/Ex048/C#.ex48.cs
@@ -1,19 +1,35 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 */
 
 using System;
 
-class Ex1{
+class Ex48{
+
+  static int START = 1;
+  static int STOP = 1000;
+
+
   static void Main(){
-    int sum = 0;
-    for(int i=1; i<1000; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i;
-      }
+    long totsum = 0;
+    for(long i=START; i<=STOP; i++){
+      totsum = (totsum + powerof(i)) % (long)1e10;
     }
-    Console.WriteLine(sum); // 233,168
+    Console.WriteLine(totsum);  // 9,110,846,700
   }
+
+
+  static long powerof(long n){
+    // fetch the last digits from the result n^n
+    long result = n;
+    for(int i=2; i<=n; i++){
+      result *= n;
+      // only keep the last digits
+      result %= (long)1e10;
+    }
+    return result;
+  }
+
 }
