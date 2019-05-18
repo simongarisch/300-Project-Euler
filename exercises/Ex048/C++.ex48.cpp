0c8dccc77b2d39fc4390bad4137b4080d68c76f8
simongarisch <gatman946@gmail.com>
2019-05-19 07:13:49 +1000

    Updating C ex48

--- exercises/Ex048/C++.ex48.cpp
+++ exercises/Ex048/C++.ex48.cpp
@@ -1,18 +1,37 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 */
 
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#define START 1
+#define STOP 1000
+
+long powerof(int n);
+
+using namespace std;
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
-    }
+  long totsum = 0;
+  for(int i=START; i<=STOP; i++){
+    totsum = (totsum + powerof(i)) % (long)1e10;
   }
-  std::cout << sum << std::endl; // 233,168
+  printf("%ld\n", totsum);  // 9,110,846,700
   return 0;
+}
+
+
+long powerof(int n){
+  // fetch the last digits from the result n^n
+  long result = n;
+  for(int i=2; i<=n; i++){
+    result = result * n;
+    // only keep the last digits
+    result = result % (long)1e10;
+  }
+  return result;
 }
--- exercises/Ex048/C.ex48.c
+++ exercises/Ex048/C.ex48.c
@@ -5,7 +5,6 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 */
 
 #include <stdio.h>
-#include <string.h>
 
 #define START 1
 #define STOP 1000
