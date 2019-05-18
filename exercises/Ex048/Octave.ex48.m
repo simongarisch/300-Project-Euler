fa26aaf3c8ea67cbd0ad1d5a8f0fafef492f7ca4
simongarisch <gatman946@gmail.com>
2019-05-18 18:07:00 +1000

    Adding Octave ex48

--- exercises/Ex048/Octave.ex48.m
+++ exercises/Ex048/Octave.ex48.m
@@ -2,14 +2,21 @@
 The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
-pkg install -forge symbolic
 %}
-
-pkg load symbolic
-
 START = 1;
 STOP = 1000;
-vec = vpa(START:STOP);
-res = strsplit(char(sum(vec.^vec)), "."){1};
 
-disp(res(end-9:end))
+function retval = powerof(n)
+  retval = n;
+  for i = 2:n
+    retval *= n;
+    # only keep the last digits
+    retval = mod(retval, 1e12);
+  endfor
+endfunction
+
+x = START:STOP;
+powers = arrayfun(@(x) powerof(x), x);
+res = num2str(sum(powers))(end-9:end);
+
+disp(res)  # 9,110,846,700
