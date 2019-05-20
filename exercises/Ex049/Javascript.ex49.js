51ba88a6c2b5763c30bace544b4206f52e8c1ac6
simongarisch <gatman946@gmail.com>
2019-05-21 06:52:04 +1000

    Adding JS ex49

--- exercises/Ex049/Javascript.ex49.js
+++ exercises/Ex049/Javascript.ex49.js
@@ -1,14 +1,77 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The arithmetic sequence, 1487, 4817, 8147,
+in which each of the terms increases by 3330, is unusual in two ways:
+(i) each of the three terms are prime, and,
+(ii) each of the 4-digit numbers are permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+exhibiting this property, but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this sequence?
 */
 
-var sum = 0;
-for(var i=0; i<1000; i++){
-  if(i % 3== 0 || i % 5 == 0){
-    sum += i;
+let START = 1488  // look for the next one
+let STOP = 1e4
+let INCREMENT = 3330
+
+function isprime(x){
+  // will return true if number is prime, false otherwise
+  if(x<2){
+    return false;
+  }
+  if(x==2){
+    return true;
+  }
+
+  for(var i=2; i<=Math.pow(x, 0.5); i++){
+    if(x % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+function ispermutation(a, b){
+  let astr = a.toString().split("").sort().join("")
+  let bstr = b.toString().split("").sort().join("")
+  return astr == bstr
+}
+
+
+function primeperms(){
+  let primes = [];
+  for(let i=START; i<=STOP; i++){
+    if(isprime(i)){
+      primes.push(i);
+    }
+  }
+
+  let numprimes = primes.length;
+  for(let idx1=0; idx1<numprimes; idx1++){
+    let prime1 = primes[idx1];
+    for(let idx2=idx1+1; idx2<numprimes; idx2++){
+      let prime2 = primes[idx2];
+      for(let idx3=idx2+1; idx3<numprimes; idx3++){
+        let prime3 = primes[idx3];
+        // check for a constant increment
+        if(prime1 + INCREMENT == prime2){
+          if(prime2 + INCREMENT == prime3){
+            // and that these are permutations
+            if(ispermutation(prime1, prime2)){
+              if(ispermutation(prime2, prime3)){
+                let primestr1 = prime1.toString();
+                let primestr2 = prime2.toString();
+                let primestr3 = prime3.toString();
+                return [primestr1, primestr2, primestr3].join("");
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 
-console.log(sum); // 233,168
+
+console.log(primeperms())  // 296,962,999,629
