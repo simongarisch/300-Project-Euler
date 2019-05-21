1e8b72cf0d0843fa3f175090bec3a7c0f3758f9a
simongarisch <gatman946@gmail.com>
2019-05-22 07:24:58 +1000

    Adding Octave ex49

--- exercises/Ex049/Octave.ex49.m
+++ exercises/Ex049/Octave.ex49.m
@@ -1,14 +1,38 @@
 %{
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The arithmetic sequence, 1487, 4817, 8147,
+in which each of the terms increases by 3330, is unusual in two ways:
+(i) each of the three terms are prime, and,
+(ii) each of the 4-digit numbers are permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+exhibiting this property, but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this sequence?
 %}
 
-sum = 0;
-for i = 1:999
-  if(mod(i,3) == 0 || mod(i,5) == 0)
-    sum += i;
-  end
-end
+START = 1488;  # look for the next one
+STOP = 1e4;
+INCREMENT = 3330;
 
-disp(sum) # 233,168
+
+function retval = ispermutation(a, b)
+  retval = strcmp(sort(num2str(a)), sort(num2str(b)));
+endfunction
+
+primes = (START:STOP)(find(isprime(START:STOP)));
+numprimes = length(primes);
+
+# the first condition is the increment
+cond1 = primes(find(ismember(primes+INCREMENT, primes) & ismember(primes+2*INCREMENT, primes)));
+for i = cond1
+  if ispermutation(i, i+INCREMENT)
+    if ispermutation(i, i+2*INCREMENT)
+      p1 = i;
+      break;
+    endif
+  endif
+endfor
+
+result = strcat(num2str(p1), num2str(p1+INCREMENT), num2str(p1+2*INCREMENT));
+result = str2num(result);
+disp(result)  # 296,962,999,629
