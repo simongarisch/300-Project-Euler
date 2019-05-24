c25d4e39f59071c253e6a31bddd9ba0e4912a5d6
simongarisch <gatman946@gmail.com>
2019-05-24 12:50:57 +1000

    Working on Python ex50

--- exercises/Ex050/Python.ex50.py
+++ exercises/Ex050/Python.ex50.py
@@ -1,12 +1,37 @@
 '''
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The prime 41, can be written as the sum of six consecutive primes:
+
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
 '''
+import sympy
 
-sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
+MAXPRIME = int(1e6)
 
-print(sum) # 233,168
+
+# collect primes from largest to smallest
+primes = list(reversed(list(sympy.primerange(0, MAXPRIME))))
+numprimes = len(primes)
+maxcounter, maxprime = 0, 0
+
+for i in range(numprimes):
+    prime = primes[i]
+    start = i
+    for j in range(start+1, numprimes):
+        counter = 0
+        totsum = 0
+        while totsum < prime:
+            if j + counter >= numprimes:
+                break
+            totsum += primes[j+counter]
+            counter += 1
+        if totsum == prime:
+            if counter > maxcounter:
+                maxcounter = counter
+                maxprime = prime
+
+print(maxprime)
