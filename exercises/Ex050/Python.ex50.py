65f2f749ee19782c6a8f6acc13caf82f63c02c63
simongarisch <gatman946@gmail.com>
2019-05-25 06:25:08 +1000

    Working ex50

--- exercises/Ex050/Python.ex50.py
+++ exercises/Ex050/Python.ex50.py
@@ -10,16 +10,56 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
 '''
 import sympy
 
-MAXPRIME = int(1e6)
+MAXPRIME = int(1e2)
 
 
 # collect primes from largest to smallest
-primes = list(reversed(list(sympy.primerange(0, MAXPRIME))))
-found = False
-maxcounter, maxprime = 0, 0
+primes = list(sympy.primerange(0, MAXPRIME))
+primeslen = len(primes)
 
-numprimes = len(primes)
-counter = numprimes
+primes_cumsum = [primes[0]]
+for i in range(1, primeslen):
+    primes_cumsum.append(primes_cumsum[i-1] + primes[i])
+
+for i in range(primeslen):
+    print(primes[i], primes_cumsum[i])
+print("---")
+
+found = False
+which_prime = 0
+which_len = 0
+i, j = 0, primeslen - 1
+
+while not found:
+    diff = primes_cumsum[j] - primes_cumsum[i]
+    if diff in primes:
+        found = True
+        which_prime = diff
+        which_len = j - i
+        break
+    i += 1
+    j -= 1
+    if i == j:
+        break
+
+print(which_prime, which_len)
+'''
+for i in range(primeslen):
+    for j in range(primeslen-1, i, -1):
+        diff = reversed_cumsum[i] - reversed_cumsum[j]
+        print(diff,  reversed_cumsum[i], reversed_cumsum[j])
+        if diff in primes:
+            which_prime = diff
+            found = True
+            break
+    if found:
+        break
+
+print(which_prime)  #
+'''
+
+
+'''
 while not found:
     counter -= 1
     for i in range(numprimes-counter):
@@ -29,7 +69,7 @@ while not found:
             found = True
             maxcounter = counter
             maxprime = slicesum
-
+'''
 '''
 for i in range(numprimes):
     prime = primes[i]
@@ -47,4 +87,3 @@ for i in range(numprimes):
                 maxcounter = counter
                 maxprime = prime
 '''
-print(maxprime)
