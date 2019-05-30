fdd06aa1483f7e6af53da73f43a53630eec0b728
simongarisch <gatman946@gmail.com>
2019-05-31 08:44:49 +1000

    Adding C ex50

--- exercises/Ex050/C.ex50.c
+++ exercises/Ex050/C.ex50.c
@@ -1,18 +1,129 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The prime 41, can be written as the sum of six consecutive primes:
+
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAXPRIME 1e6
+
+
+struct Primes {
+   long *arr;
+   long len;
+};
+
+
+// forward declarations
+int isprime(long x);
+struct Primes collectPrimes(long maxprime);
+long whichPrime();
+int primesContains(struct Primes primes, long n);
+
 
 int main(){
-  int sum = 0;
-  for(int i=1; i<1000; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum = sum + i;
+  long prime = whichPrime();
+  printf("%ld\n", prime);  // 997,651
+  return 0;
+}
+
+
+long whichPrime(){
+  // Which prime, below one-million,
+  // can be written as the sum of the most consecutive primes?
+  struct Primes primes = collectPrimes(MAXPRIME);
+  struct Primes cumsum;
+
+  cumsum.len = primes.len + 1;
+  cumsum.arr = (long*)calloc(cumsum.len, sizeof(long));
+  long total = 0;
+  for(int i=0; i<primes.len; i++){
+    total = total + primes.arr[i];
+    cumsum.arr[i+1] = total;
+  }
+  //for(int i=0; i<primes.len; i++){
+  //  printf("%ld, %ld\n", primes.arr[i], cumsum.arr[i+1]);
+  //}
+
+  int found = FALSE;
+  long whichPrime = 0;
+  long whichLen = primes.len;
+  long diff;
+
+  while(!found){
+    for(int i=0; i < primes.len - whichLen + 1; i++){
+      diff = cumsum.arr[i+whichLen] - cumsum.arr[i];
+      if(diff > MAXPRIME){
+        break;
+      }
+      if(primesContains(primes, diff)){
+        found = TRUE;
+        whichPrime = diff;
+        break;
+      }
+    }
+    if(found){
+      break;
+    }
+    whichLen = whichLen - 1;
+    if(whichLen <= 0){
+      break;
     }
   }
-  printf("%d\n", sum); // 233,168
-  return 0;
+
+  free(primes.arr);
+  free(cumsum.arr);
+  return whichPrime;
+}
+
+
+int primesContains(struct Primes primes, long n){
+  for(int i=0; i<primes.len; i++){
+    if(primes.arr[i] == n){
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
+
+
+struct Primes collectPrimes(long maxprime){
+  struct Primes primes;
+  primes.arr = (long *)malloc(MAXPRIME * sizeof(long));
+  primes.len = 0;
+  for(long i=1; i<=MAXPRIME; i++){
+    if(isprime(i)){
+      primes.len++;
+      primes.arr[primes.len-1] = i;
+    }
+  }
+  primes.arr = (long*)realloc(primes.arr, (primes.len)*sizeof(long));
+  return primes;
+}
+
+
+int isprime(long x){
+  // will return true if x is prime, false otherwise
+  if(x < 2){
+    return FALSE;
+  }
+  if(x == 2){
+    return TRUE;
+  }
+  for(long i=2; i<=pow(x, 0.5); i++){
+    if(x % i == 0){
+      return FALSE;
+    }
+  }
+  return TRUE;
 }
