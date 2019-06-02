8e1d1cfaeda0f167ce1b1903f92cfbde282b0313
simongarisch <gatman946@gmail.com>
2019-06-03 06:22:48 +1000

    Working on Java ex50

--- exercises/Ex050/JavaEx50.java
+++ exercises/Ex050/JavaEx50.java
@@ -1,19 +1,91 @@
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+The prime 41, can be written as the sum of six consecutive primes:
+
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
 */
 
-public class JavaEx1 {
+public class JavaEx50{
+
+    static long MAXPRIME = (long)1e6;
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i=1; i<1000; i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            sum += i;
+        System.out.println(whichPrime());  //
+    }
+
+
+    static long whichPrime(){
+      // Which prime, below one-million,
+      // can be written as the sum of the most consecutive primes?
+      List<Long> primes = collectPrimes(MAXPRIME);
+      List<Long> cumsum = new ArrayList<Long>();
+      cumsum.add(0);
+
+      long total = 0;
+      for(int i=0; i<primes.size(); i++){
+        total += prime;
+        cumsum.add(total);
+      }
+
+      bool found = false;
+      long primesLen = primes.Count;
+      long whichPrime = 0;
+      long whichLen = primesLen;
+      long diff = 0;
+
+      while(!found){
+        for(int i=0; i < primesLen - whichLen + 1; i++){
+          diff = cumsum[i+(int)whichLen] - cumsum[i];
+          if(diff > MAXPRIME){
+            break;
+          }
+          if(primes.Contains(diff)){
+            found = true;
+            whichPrime = diff;
+            break;
           }
         }
-        System.out.println(sum); // 233,168
+        if(found){
+          break;
+        }
+        whichLen -= 1;
+        if(whichLen <= 0){
+          break;
+        }
+      }
+      return whichPrime;
+    }
+
+
+    static List<Long> collectPrimes(long maxprime){
+      List<Long> primes = new ArrayList<Long>();
+      for(var i=1; i<maxprime; i++){
+        if(isprime(i)){
+          primes.add(i);
+        }
+      }
+      return primes;
+    }
+
+
+    public static boolean isprime(long x){
+      // will return true if x is prime, false otherwise
+      if(x < 2){
+        return false;
+      }
+      if(x == 2){
+        return true;
+      }
+      for(long i=2; i<=Math.pow(x, 0.5); i++){
+        if(x % i == 0){
+          return false;
+        }
+      }
+      return true;
     }
 
 }
