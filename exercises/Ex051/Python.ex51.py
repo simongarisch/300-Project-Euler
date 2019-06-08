8f5c2e0363845489937eae4a50ffffe9eaba5879
simongarisch <gatman946@gmail.com>
2019-06-08 17:14:27 +1000

    Adding Python ex51

--- exercises/Ex051/Python.ex51.py
+++ exercises/Ex051/Python.ex51.py
@@ -16,6 +16,7 @@ is part of an eight prime value family.
 '''
 
 import itertools
+from bisect import bisect_left
 from collections import Counter
 import sympy
 
@@ -24,146 +25,82 @@ STOP = int(1e6)
 TARGET_PRIMES = 7
 DIGITS = "0123456789"
 
-primes = list(sympy.primerange(START, STOP))
-strprimes = [str(prime) for prime in primes]
-smallest_prime = 0
 
-# get the indices we need to replace with * in our search
-replacements_dict = {}
-for n in range(2, len(str(STOP))):
+def collect_primes(start, stop):
+    # collect all prime numbers within some range
+    return list(sympy.primerange(start, stop))
+#print(collect_primes(0, 20))  # [2, 3, 5, 7, 11, 13, 17, 19]
+
+
+def collect_digit_replacements(ndigits):
+    # we'll have to replace certain digits with a *
+    # get combinations of digits we'll need to replace
     combs = []
-    for i in range(1, min(n, 3)):
-        combs.extend(list(itertools.combinations(range(n), i)))
-    replacements_dict[n] = combs
-#print(replacements_dict[2])  # [(0,), (1,)]
-#print(replacements_dict[3])  # [(0,), (1,), (2,), (0, 1), (0, 2), (1, 2)]
+    for i in range(1, ndigits):
+        combs.extend(list(itertools.combinations(range(ndigits), i)))
+    return combs
+#print(collect_digit_replacements(2))  # [(0,), (1,)]
+#print(collect_digit_replacements(3))  # [(0,), (1,), (2,), (0, 1), (0, 2), (1, 2)]
 
-# start with two digits and work our way up
-found = False
-ndigits = 1
-while not found:
-    ndigits += 1
-    strprimes_filtered = []
-    for strprime in strprimes:
-        strlen = len(strprime)
-        if strlen == ndigits:
-            strprimes_filtered.append(strprime)
-        if strlen > ndigits:
-            break
 
-    replacements = replacements_dict[ndigits]
+def filter_ndigit(primes_list, n):
+    # return only numbers in the list with n digits
+    # also return numbers with combinations of * inserted
+    primes_filtered = [prime for prime in primes_list if len(str(prime)) == n]
+    primes_strings = [str(prime) for prime in primes_filtered]
+    replacements = collect_digit_replacements(n)
+
     stars = []
-    for index, prime in enumerate(strprimes_filtered):
+    for index, prime in enumerate(primes_strings):
         for replacement in replacements:
-            prime_list = list(prime)
+            plist = list(prime)
             for index in replacement:
-                prime_list[index] = "*"
-            stars.append("".join(prime_list))
+                plist[index] = "*"
+            stars.append("".join(plist))
+    return primes_filtered, stars
+#primes_list = collect_primes(0, int(1e2))
+#primes_filtered, stars = filter_ndigit(primes_list, 2)
+#print(primes_filtered)  # [11, 13, 17, 19, 23, 29, 31, 37, 41 ...
+#print(stars)            # ['*1', '1*', '*3', '1*', '*7', '1*', ...
 
-    # get a dictionary with the number of times a star (such as '*3') occurs
+
+def find_elem(elem, sorted_list):
+    # https://docs.python.org/3/library/bisect.html
+    i = bisect_left(sorted_list, elem)
+    if i != len(sorted_list) and sorted_list[i] == elem:
+        return True
+    return False
+#print(find_elem(5, list(range(10))))  # True
+
+
+def ndigit_family(primes_list, n, target_primes):
+    # check for unique pattern groups with n or more digits
+    primes_filtered, stars = filter_ndigit(primes_list, n)
     counter = Counter(stars)
-    counter = {k:v for k,v in counter.iteritems() if v == TARGET_PRIMES}
-    print(counter)
-    if len(counter) != 0:  # we have matches for the target primes
-        smallest_prime = STOP
-        section = []
-        numbers = []
-        for key, value in counter.iteritems():
-            for digit in DIGITS:
-                value_rep = int(key.replace("*", digit))
-                if len(str(value_rep)) == ndigits:
-                    if value_rep in primes:
-                        section.append(value_rep)
-            if len(section) == TARGET_PRIMES:
-                numbers.append(section)
-                smallest_prime = min(smallest_prime, min(section))
-
-        if len(numbers) > 0:
-            print(numbers)
-            found = True
-
-print(smallest_prime)  #
-'''
-import itertools
-import sympy
-
-START = int(56e3)
-STOP = int(1e6)
-DIGITS = "0123456789"
-DIGITS_EX0 = "123456789"
-TARGET_PRIMES = 7
+    counter = {k:v for k,v in counter.iteritems() if v >= target_primes}
+    min_all = []  # the minimum prime from all groups
+    for key in counter.keys():
+        count_primes = 0
+        primes = []
+        for digit in DIGITS:
+            value = int(key.replace("*", digit))
+            if len(str(value)) == n:
+                if find_elem(value, primes_filtered):
+                    count_primes += 1
+                    primes.append(value)
+        if count_primes == target_primes:
+            min_all.append(min(primes))
+    return min(min_all) if len(min_all) > 0 else None
+#print(ndigit_family(collect_primes(0, int(1e3)), 2, 6))  # 13
+#print(ndigit_family(collect_primes(0, int(1e6)), 5, 7))  # 56003
 
 
-primes = list(sympy.primerange(START, STOP))
+primes_list = collect_primes(START, STOP)
 
+ndigits = 1
+result = None
+while result is None:
+    ndigits += 1
+    result = ndigit_family(primes_list, ndigits, 8)
 
-def primes_in_family(family="*3"):
-    global primes, DIGITS
-    counter = 0
-    first = None
-    if family[0] == "*":
-        digits = DIGITS_EX0
-    else:
-        digits = DIGITS
-
-    for digit in digits:
-        n = int(family.replace("*", digit))
-        if n in primes:
-            if counter == 0:
-                first = n
-            counter += 1
-    return counter, first
-#print(primes_in_family())  # (6, 13)
-
-
-replacements_dict = {}
-def replace_index(n):
-    # get the indices of digits to replace
-    # only look to replace 1-2 digits at this stage
-    global replacements_dict
-    nlen = len(str(n))
-    replacements = replacements_dict.get(nlen)
-    if replacements is not None:
-        return replacements
-
-    rep1 = list(itertools.combinations(range(nlen), 1))
-    rep2 = []
-    if nlen > 2:
-        rep2 = list(itertools.combinations(range(nlen), 2))
-    rep1.extend(rep2)
-
-    replacements = rep1[:]
-    replacements_dict[nlen] = replacements
-    return replacements
-#print(replace_index(222))  # [(0,), (1,), (2,), (0, 1), (0, 2), (1, 2)]
-
-
-num_primes = len(primes)
-primes_index = 0  # start checking from the first prime
-checked = []      # all the numbers we have checked
-found = False
-
-while not found and primes_index < num_primes:
-    numstr = str(primes[primes_index])
-    replacements = replace_index(numstr)
-    for replacement in replacements:
-        family = [i for i in numstr]
-        for index in replacement:
-            family[index] = "*"
-        family = "".join(family)
-        if family not in checked:
-            checked.append(family)
-            counter, first = primes_in_family(family)
-            if counter == TARGET_PRIMES:
-                print(family)
-                found = True
-                break
-
-    if found:
-        break
-    else:
-        first = 0
-    primes_index += 1
-
-print(first)
-'''
+print(result)  # 121,313
