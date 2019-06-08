d78193a40cae9811c9ec3541306ca51200a1716a
simongarisch <gatman946@gmail.com>
2019-06-09 06:53:01 +1000

    Working on R ex51

--- exercises/Ex051/R.ex51.R
+++ exercises/Ex051/R.ex51.R
@@ -1,13 +1,81 @@
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# By replacing the 1st digit of the 2-digit number *3,
+# it turns out that six of the nine possible values: 13, 23, 43, 53, 73, and 83,
+# are all prime.
+#
+# By replacing the 3rd and 4th digits of 56**3 with the same digit,
+# this 5-digit number is the first example having seven primes among the ten
+# generated numbers, yielding the family:
+# 56003, 56113, 56333, 56443, 56663, 56773, and 56993.
+# Consequently 56003, being the first member of this family,
+# is the smallest prime with this property.
+#
+# Find the smallest prime which, by replacing part of the number
+# (not necessarily adjacent digits) with the same digit,
+# is part of an eight prime value family.
+library("arrangements")
 
-sum <- 0
-for(i in 1:999){
-  if((i %% 3 == 0) || (i %% 5 == 0)){
-    sum <- sum + i
+START = 1
+STOP = 1e6
+TARGET_PRIMES = 8
+DIGITS = "0123456789"
+
+
+isprime <- function(x){
+  if(x < 2){
+    return(FALSE)
   }
+  if(x == 2){
+    return(TRUE)
+  }
+  for(i in 2:as.integer(x ^ 0.5 + 1)){
+    if(x %% i == 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
 }
 
-print(sum) # 233,168
+
+collect_primes <- function(start, stop){
+  primes <- c()
+  for(i in start:stop){
+    if(isprime(i)){
+      primes <- c(primes, i)
+    }
+  }
+  return(primes)
+}
+#print(collect_primes(0, 20))  # [1]  2  3  5  7 11 13 17 19
+
+
+collect_digit_replacements <- function(ndigits){
+  # we'll have to replace certain digits with a *
+  # get combinations of digits we'll need to replace
+  combs <- list()
+  counter <- 0
+  indices <- 1:ndigits
+  for(i in indices){
+    counter <- counter + 1
+    a <- arrangements::combinations(indices, i, replace=FALSE)
+    combs[[counter]] <- a
+  }
+  return(combs)
+}
+#print(collect_digit_replacements(2))  # list of matrices
+
+
+filter_ndigit <- function(primes, n){
+  primes_filtered <- c()
+  primes_strings <- c()
+  for(i in 1:length(primes)){
+    prime <- primes[i]
+    if(nchar(as.character(prime))==n){
+      primes_filtered <- c(primes_filtered, prime)
+      primes_strings <- c(primes_strings, as.character(prime))
+    }
+  }
+
+  replacements = collect_digit_replacements(n)
+  stars <- c()
+}
