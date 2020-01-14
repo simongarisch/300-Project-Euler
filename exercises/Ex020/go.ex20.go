3166796f09554568dfcef5794af31cd58f414d89
simongarisch <gatman946@gmail.com>
2020-01-15 06:14:58 +1100

    adding go ex20

--- exercises/Ex020/go.ex20.go
+++ exercises/Ex020/go.ex20.go
@@ -3,23 +3,36 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"strconv"
 )
 
-func fact(n big.Int) big.Int {
+func fact(n *big.Int) *big.Int {
 	result := big.NewInt(1)
-	stopCondition := big.NewInt(1)
+	one := big.NewInt(1)
+
 	for {
-		if n == stopCondition {
+		if n.Cmp(one) == 0 {
 			break
 		}
-		result = result * n
-		n--
+
+		result.Mul(result, n)
+		n.Sub(n, one)
 	}
 	return result
 }
 
 func main() {
-	n := big.NewInt(100)
-	fmt.Println(fact(n))
+	numstr := fact(big.NewInt(100)).String()
+	//fmt.Println(numstr) // 93326215 ...
 
+	sum := 0
+	for _, c := range numstr {
+		i, err := strconv.Atoi(string(c))
+		if err != nil {
+			panic(err)
+		}
+		sum += i
+	}
+
+	fmt.Println(sum) // 648
 }
