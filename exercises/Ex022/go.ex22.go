04ffa47c290e0df6c54e03996e9eecdd062a2fca
simongarisch <gatman946@gmail.com>
2020-01-15 13:54:09 +1100

    adding go ex22

--- exercises/Ex022/go.ex22.go
+++ exercises/Ex022/go.ex22.go
@@ -16,12 +16,47 @@ func getNames() ([]string, error) {
 	}
 
 	cleanNames := strings.Replace(string(data), "\"", "", -1)
+	cleanNames = strings.ToLower(cleanNames)
 	names = strings.Split(cleanNames, ",")
 	sort.Strings(names)
-	fmt.Println(names)
 	return names, nil
 }
 
+func createNameScoreLookup() func(string) int {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	scores := make(map[string]int)
+	for index, char := range alphabet {
+		scores[string(char)] = index + 1
+	}
+
+	return func(name string) int {
+		score := 0
+		for _, char := range name {
+			score += scores[string(char)]
+		}
+		return score
+	}
+}
+
 func main() {
-	getNames()
+	names, err := getNames()
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println(names)
+
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	scores := make(map[string]int)
+	for index, char := range alphabet {
+		scores[string(char)] = index + 1
+	}
+
+	nameScoreLookup := createNameScoreLookup()
+	totalScore := 0
+	for index, name := range names {
+		nameScore := nameScoreLookup(name)
+		totalScore += (index + 1) * nameScore
+	}
+
+	fmt.Println(totalScore) // 871,198,282
 }
