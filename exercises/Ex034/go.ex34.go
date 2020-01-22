e23b2e1b19293b04d868e53c7c2b227ca516426d
simongarisch <gatman946@gmail.com>
2020-01-23 06:10:32 +1100

    working on ex34

---
+++ exercises/Ex034/go.ex34.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"strconv"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+// returns true if x is equal to the sum its digit factorials,
+// false otherwise
+func isCurious(x int) (bool, error) {
+	sumfact := 0
+	xstr := strconv.Itoa(x)
+	for _, char := range xstr {
+		schar := string(char)
+		ichar, err := strconv.Atoi(schar)
+		if err != nil {
+			return false, err
+		}
+		sumfact += factorial(ichar)
+	}
+
+	if x == sumfact {
+		return true, nil
+	}
+	return false, nil
+}
+
+func main() {
+	//fmt.Println(isCurious(145))  // true <nil>
+}
