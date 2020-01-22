0d3efdcb1660aad60c799e1fecbbab399694a0c9
simongarisch <gatman946@gmail.com>
2020-01-23 06:46:56 +1100

    adding to ex34

--- exercises/Ex034/go.ex34.go
+++ exercises/Ex034/go.ex34.go
@@ -11,6 +11,17 @@ func factorial(n int) int {
 	return n * factorial(n-1)
 }
 
+func pow(a, b int) int {
+	if b == 0 {
+		return 1
+	}
+	result := a
+	for i := 2; i <= b; i++ {
+		result *= a
+	}
+	return result
+}
+
 // returns true if x is equal to the sum its digit factorials,
 // false otherwise
 func isCurious(x int) (bool, error) {
